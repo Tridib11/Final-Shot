2df6fbd770df009a53a6a8f901e0a1f44be1d8cd
Tridib11 <tridibghosh12345@gmail.com>
2024-09-16 00:15:24 +0530

    File added

--- Advance Backend/Js-Pogramming/strings.js
+++ Advance Backend/Js-Pogramming/strings.js
@@ -1,4 +1,12 @@
-const name="Rohan"
-const repoCount=50
+// const name="Rohan"
+// const repoCount=50
 
-console.log(`My name is ${name} and my Repo count is ${repoCount}`)
+// console.log(`My name is ${name} and my Repo count is ${repoCount}`)
+const name=new String("Rohan")
+console.log(name)
+console.log(name.length)
+const newName=name.toLocaleUpperCase()
+console.log(name)
+console.log(newName)
+
+console.log(name.toUpperCase())

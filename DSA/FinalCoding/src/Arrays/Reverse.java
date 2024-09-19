3153dff5238b36028562abf06a3c043b964e4d0a
Tridib11 <tridibghosh12345@gmail.com>
2024-09-19 09:29:48 +0530

    File added

---
+++ DSA/FinalCoding/src/Arrays/Reverse.java
@@ -0,0 +1,22 @@
+package Arrays;
+import java.util.*;
+public class Reverse {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(reverse(n));
+
+    }
+
+    private static int reverse(int n){
+        int sum=0,d=0;
+        while(n!=0){
+            d=n%10;
+            sum=sum*10+d;
+            n=n/10;
+        }
+
+        return sum;
+    }
+}

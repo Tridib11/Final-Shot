3aec1271647a1d65a31ae3e202b420bad946d7aa
Tridib11 <tridibghosh12345@gmail.com>
2024-09-23 07:56:12 +0530

    Files added

---
+++ Advance Backend/cohort/ages.js
@@ -0,0 +1,12 @@
+const users=[1,2,3]
+const user2=["Rohan","rahul"]
+const allUsers=[{
+  fName:"Tridib",
+  gender:"Male"
+},{
+  fName:"Raman",
+  gender:"Male"
+},{
+  fName:"Swati",
+  gender:"Female"
+}]

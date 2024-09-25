2991509f1948b72ead427691bce29a3623192afc
Tridib11 <tridibghosh12345@gmail.com>
2024-09-25 08:55:52 +0530

    File adeed

---
+++ Cohort/Week1/jsapis.js
@@ -0,0 +1,9 @@
+// let str="Rohan"
+// console.log(str.length)
+
+function str(name,target){
+  console.log(`Orginal String ${name}`)
+  console.log(`Index of `,name.indexOf(target))
+}
+
+str("Hello World","World")

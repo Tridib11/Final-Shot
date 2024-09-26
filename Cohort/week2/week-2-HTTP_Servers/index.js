ff152be8a656718e481647a7c4d1416954ce00da
Tridib11 <tridibghosh12345@gmail.com>
2024-09-26 10:49:04 +0530

    first commit

--- Cohort/week2/week-2-HTTP_Servers/index.js
+++ Cohort/week2/week-2-HTTP_Servers/index.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const app=express()
 
+
+app.use(express.json())
+
+
 app.get('/',(req,res)=>{
   res.send("I am working")
 })
@@ -11,6 +15,15 @@ app.get("/json",(req,res)=>{
     name:"Tridib",
     age:21
   })
+})
+
+
+app.post("/test",(req,res)=>{
+  console.log(req.headers['authorization'])
+  console.log(req.headers)
+
+  console.log(req.body)
+  res.send("<b>Working</b>")
 })
 
 app.listen(8000,()=>{

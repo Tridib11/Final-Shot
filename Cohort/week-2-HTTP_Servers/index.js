d014c830ed2b313d6a1ac24bb67e17d06e9c2f8f
Tridib11 <tridibghosh12345@gmail.com>
2024-09-26 07:46:22 +0530

    Files added

--- Cohort/week-2-HTTP_Servers/index.js
+++ Cohort/week-2-HTTP_Servers/index.js
@@ -5,6 +5,14 @@ app.get('/',(req,res)=>{
   res.send("I am working")
 })
 
+
+app.get("/json",(req,res)=>{
+  res.json({
+    name:"Tridib",
+    age:21
+  })
+})
+
 app.listen(8000,()=>{
   console.log("Server started")
 })

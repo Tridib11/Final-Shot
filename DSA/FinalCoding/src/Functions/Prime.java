9b4797a71323e17fc1c77f5f9e6b443be0ebf0e0
Tridib11 <tridibghosh12345@gmail.com>
2024-09-27 08:45:49 +0530

    File added

--- DSA/FinalCoding/src/Functions/Prime.java
+++ DSA/FinalCoding/src/Functions/Prime.java
@@ -19,12 +19,11 @@ public class Prime {
     }
 
     private static boolean isPrime(int n){
-        int count=0;
-        for(int i=2;i<=n;i++){
+        for(int i=2;i<=Math.sqrt(n);i++){
             if(n%i==0){
-                count++;
+                return false;
             }
         }
-        return count<=1?true:false;
+        return true;
     }
 }

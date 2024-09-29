1995cdbe13cabe3205adc1d283a8f01af005eefa
Tridib11 <tridibghosh12345@gmail.com>
2024-09-29 22:56:13 +0530

    File added

--- Cohort/week3/test.js
+++ Cohort/week3/test.js
@@ -1,16 +1,32 @@
 const express=require("express")
 const app=express()
 
-app.get("/health-checkup",(req,res)=>{
+
+function middleWare(req,res,next){
   const username=req.headers.username
   const password=req.headers.password
-  const id=req.headers.id
+
   if(username!="Tridib" || password!="pass"){
     return res.status(403).json({"message": "Wrong inputs"})
+  }else{
+    next()
   }
+}
+
+
+function kidneyId(req,res,next){
+  const id=req.headers.id
+ 
   if(id!=1 && id!=2){
     return res.status(400).json({"message": "Wrong inputs"})
   }
+  else{
+    next()
+  }
+}
+
+app.get("/health-checkup",middleWare,kidneyId,(req,res)=>{
+  
   res.json({"message": "Your kidneys are fine"})
 })
 

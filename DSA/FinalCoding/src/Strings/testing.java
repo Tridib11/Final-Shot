23f291dce9af5da0bce6ffa0e9577b16111f2056
Tridib11 <tridibghosh12345@gmail.com>
2024-10-01 10:45:07 +0530

    File added

---
+++ DSA/FinalCoding/src/Strings/testing.java
@@ -0,0 +1,14 @@
+package Strings;
+
+public class testing {
+    public static void main(String[] args) {
+        String a="Rohan";
+        String b="Rohan";
+        System.out.println(a==b);
+
+        String c=new String("Rohan");
+        String d=new String("Rohan");
+        System.out.println(c==d);
+        System.out.println(c.equals(d));
+    }
+}

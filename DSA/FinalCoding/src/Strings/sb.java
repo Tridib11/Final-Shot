ed2128c562ccdb59b1494d653635d4fe51d85795
Tridib11 <tridibghosh12345@gmail.com>
2024-10-02 09:53:51 +0530

    File added

--- DSA/FinalCoding/src/Strings/sb.java
+++ DSA/FinalCoding/src/Strings/sb.java
@@ -2,11 +2,17 @@ package Strings;
 
 public class sb {
     public static void main(String[] args) {
-        StringBuilder builder=new StringBuilder();
-        for(int i=0;i<26;i++){
-            char c=(char)('a'+i);
-            builder.append(c);
-        }
-        System.out.println(builder);
+//        StringBuilder builder=new StringBuilder();
+//        for(int i=0;i<26;i++){
+//            char c=(char)('a'+i);
+//            builder.append(c);
+//        }
+//        System.out.println(builder);
+
+
+        int a=5;
+        int b=7;
+        int res=a++ + b;
+        System.out.println(res);
     }
 }

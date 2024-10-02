6e55debe42420cf29b5146e5f055aff23e07a2a0
Tridib11 <tridibghosh12345@gmail.com>
2024-10-02 07:53:19 +0530

    File added

--- DSA/FinalCoding/src/Strings/characterarray.java
+++ DSA/FinalCoding/src/Strings/characterarray.java
@@ -2,6 +2,12 @@ package Strings;
 
 public class characterarray {
     public static void main(String[] args) {
-
+        String s="";
+        for(int i=0;i<=26;i++){
+            char ch=(char)('a'+i);
+            s+=ch;
+            System.out.println(s);
+        }
+        System.out.println(s);
     }
 }

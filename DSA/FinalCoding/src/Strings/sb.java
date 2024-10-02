7199ccfdf5f8d6b25394c2cdde921e3a5c8e5f4c
Tridib11 <tridibghosh12345@gmail.com>
2024-10-02 07:57:51 +0530

    File added

---
+++ DSA/FinalCoding/src/Strings/sb.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class sb {
+    public static void main(String[] args) {
+        StringBuilder builder=new StringBuilder();
+        for(int i=0;i<26;i++){
+            char c=(char)('a'+i);
+            builder.append(c);
+        }
+        System.out.println(builder);
+    }
+}

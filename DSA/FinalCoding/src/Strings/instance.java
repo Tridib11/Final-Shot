d075c17609c5c2de5b5501f9814addbd07c74fdc
Tridib11 <tridibghosh12345@gmail.com>
2024-10-04 12:39:23 +0530

    File added

---
+++ DSA/FinalCoding/src/Strings/instance.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class instance {
+    public static void main(String[] args) {
+        String s1=new String("ROhan");
+        boolean ans=s1 instanceof String;
+        System.out.println(ans);
+    }This is a sample text for plagiarism detection. This is an original sentence that should not be flagged. Another example text to check for plagiarism. This is another original sentence. More texts can be added here for comparison.
+}
+
+
+

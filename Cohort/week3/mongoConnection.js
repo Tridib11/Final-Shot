2bd1d5a4db450bd2c8ac397e98a428435be238ea
Tridib11 <tridibghosh12345@gmail.com>
2024-10-09 02:37:05 +0530

    file added

--- Cohort/week3/mongoConnection.js
+++ Cohort/week3/mongoConnection.js
@@ -1,22 +1,48 @@
+const express = require("express")
 const mongoose=require("mongoose")
+const app=express()
+app.use(express())
 
 mongoose.connect("mongodb+srv://admin:admin@cluster0.pcgvjbl.mongodb.net/userappnew")
 
 const User=mongoose.model(
   'Users',
   {
-    name:String,
+    username:String,
     email:String,
     password:String
   }
 )
 
 
-const user=new User({
-  name:"Tridib",
-  email:"tridibghosh12345@gmail.com",
-  password:"123456"
+app.use(express.json())
+app.post("/signup",async(req,res)=>{
+  const username=req.body.username
+  const password=req.body.password
+  const email=req.body.email
+
+  const existingUser=await User.findOne({email:email})
+  if(existingUser){
+    return res.status(403).json({
+      msg:"Email already exists"
+    })
+  }
+  const user=new User({
+    username,
+    email,
+    password
+  })
+  
+  
+  user.save()
+
+  return res.status(201).json({
+    msg:"User created successfully"
+  })
 })
 
 
-user.save()
+
+app.listen(8000,()=>{
+  console.log("Server started")
+})

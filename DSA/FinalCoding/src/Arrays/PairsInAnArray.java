1934f7a5d2872ee50efb7bfb608217793c9d4bcb
Tridib11 <tridibghosh12345@gmail.com>
2024-10-11 01:14:46 +0530

    File added

--- DSA/FinalCoding/src/Arrays/PairsInAnArray.java
+++ DSA/FinalCoding/src/Arrays/PairsInAnArray.java
@@ -2,6 +2,16 @@ package Arrays;
 
 public class PairsInAnArray {
     public static void main(String[] args) {
+        int[] arr={2,4,6,8,10};
+        pairs(arr);
+    }
 
+    private static void pairs(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print(" ("+arr[i]+","+arr[j]+")");
+            }
+            System.out.println();
+        }
     }
 }

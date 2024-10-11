a9ea308399c77b8b3c8759e49e485714f8e5858b
Tridib11 <tridibghosh12345@gmail.com>
2024-10-11 19:43:57 +0530

    File added

--- DSA/FinalCoding/src/Arrays/FindSubArraySumEqualsK.java
+++ DSA/FinalCoding/src/Arrays/FindSubArraySumEqualsK.java
@@ -2,6 +2,9 @@ package Arrays;
 import java.util.*;
 public class FindSubArraySumEqualsK {
     public static void main(String[] args) {
+        int[] arr={10,15,-5,15,-10,5};
+        int sum=5;
+        subArraySum(arr,sum);
 
     }
 
@@ -13,7 +16,56 @@ public class FindSubArraySumEqualsK {
         HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
             currSum+=arr[i];
+            if(currSum-sum==0){
+                start=0;
+                end=i;
+                break;
+            }
+            if(map.containsKey(currSum-sum)){
+                start=map.get(currSum-sum)+1;
+                end=i;
+                break;
+            }
+            map.put(currSum,i);
+        }
 
+        if(end==-1){
+            System.out.println("Not found");
+        }
+        else{
+            System.out.println(start+" "+end);
+        }
+    }
+
+
+
+    private static int subArraySumMaxLength(int[] arr,int sum){
+        int currSum=0;
+        int start=0;
+        int end=-1;
+        int maxLength=-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            currSum+=arr[i];
+            if(currSum-sum==0){
+                start=0;
+                end=i;
+                maxLength=Math.max(maxLength,(end-start)+1);
+
+            }
+            if(map.containsKey(currSum-sum)){
+                start=map.get(currSum-sum)+1;
+                end=i;
+                maxLength=Math.max(maxLength,(end-start)+1);
+            }
+            map.put(currSum,i);
+        }
+
+        if(end==-1){
+            return end;
+        }
+        else{
+            return maxLength;
         }
     }
 }

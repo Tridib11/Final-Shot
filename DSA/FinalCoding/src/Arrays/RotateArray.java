cc04cc3e3f63a86e579216d3ac0a7ff6951687d5
Tridib11 <tridibghosh12345@gmail.com>
2024-10-11 11:41:02 +0530

    File added

---
+++ DSA/FinalCoding/src/Arrays/RotateArray.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4,5,6,7};
+       rotate(arr,3);
+
+    }
+    public static void rotate(int[] nums, int k) {
+
+        reverse(nums,0,k);
+        reverse(nums,k+1,nums.length-1);
+        reverse(nums,0,nums.length-1);
+        for(int i:nums){
+            System.out.println(i);
+        }
+
+    }
+
+
+    private static void reverse(int[] arr,int start,int end){
+        while(start<=end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}

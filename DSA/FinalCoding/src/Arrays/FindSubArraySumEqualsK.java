adeb47faa3acb51998f05f92e1d0c194357a7e2f
Tridib11 <tridibghosh12345@gmail.com>
2024-10-11 19:35:52 +0530

    File added

---
+++ DSA/FinalCoding/src/Arrays/FindSubArraySumEqualsK.java
@@ -0,0 +1,19 @@
+package Arrays;
+import java.util.*;
+public class FindSubArraySumEqualsK {
+    public static void main(String[] args) {
+
+    }
+
+
+    private static void subArraySum(int[] arr,int sum){
+        int currSum=0;
+        int start=0;
+        int end=-1;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            currSum+=arr[i];
+
+        }
+    }
+}

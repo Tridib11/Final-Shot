dab40c8bb0ae295a0638fb0b1b9afebad9b3bede
Tridib11 <tridibghosh12345@gmail.com>
2024-10-14 00:11:10 +0530

    File added

---
+++ DSA/FinalCoding/src/Arrays/MaxSubArraySUm.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.*;
+public class MaxSubArraySUm {
+    public static void main(String[] args) {
+        List<Integer> list=new ArrayList<>();
+        list.add(4);
+        list.add(3);
+        list.add(1);
+        list.add(5);
+        list.add(6);
+        System.out.println(pairWithMaxSum(list));
+    }
+
+    public static int pairWithMaxSum(List<Integer> arr) {
+        // Your code goes here
+
+
+        int max_sum=arr.get(0);
+        int sum=0;
+
+        for(int i=0;i<arr.size();i++){
+            sum+=arr.get(i);
+            if(sum>max_sum){
+                max_sum=sum;
+            }
+            else if(sum<0){
+                sum=0;
+            }
+        }
+        return max_sum;
+    }
+}

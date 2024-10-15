e1db649a915f882ba18774fa308f4c279de4406a
Tridib11 <tridibghosh12345@gmail.com>
2024-10-16 01:30:20 +0530

    file added

--- Cohort/week3/assignment.js
+++ Cohort/week3/assignment.js
@@ -1,42 +1,41 @@
-const jwt=require("jsonwebtoken")
-const jwtPassword="secret"
-const zod=require("zod")
+const jwt = require("jsonwebtoken");
+const jwtPassword = "secret";
+const zod = require("zod");
 
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 
-const emailSchema=zod.string().email()
-const passwordSchema=zod.string().min(6)
+function signJwt(username, password) {
+  const userResponse = emailSchema.safeParse(username);
+  const passwordResponse = passwordSchema.safeParse(password);
 
-
-function signJwt(username,password){
-    const userResponse=emailSchema.safeParse(username)
-    const passwordResponse=passwordSchema.safeParse(password)
-
-    if (!userResponse.success || !passwordResponse.success){
-        return null
-    }
-    const signature=jwt.sign({
-        userResponse
-    },jwtPassword)
-    return signature
+  if (!userResponse.success || !passwordResponse.success) {
+    return null;
+  }
+  const signature = jwt.sign(
+    {
+      userResponse,
+    },
+    jwtPassword
+  );
+  return signature;
 }
 
-
-function decodeJwt(token){
-    const decoded=jwt.decode(token)
-    if (decoded){
-        return true
-    }else{
-        return false
-    }
+function decodeJwt(token) {
+  const decoded = jwt.decode(token);
+  if (decoded) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function verifyJwt(token){
-    try{
-        jwt.verify(token,jwtPassword)
-        return true
-    }catch (e){
-
-    }
-    return false
+function verifyJwt(token) {
+  let ans = true;
+  try {
+    jwt.verify(token, jwtPassword);
+  } catch (e) {
+    ans = false;
+  }
+  return ans;
 }
-

bbe0415b4872d5ecf7e9de1fdcc7e3ce9105497f
Tridib11 <tridibghosh12345@gmail.com>
2024-10-16 01:29:02 +0530

    file added

--- Cohort/week3/assignment.js
+++ Cohort/week3/assignment.js
@@ -1,11 +1,42 @@
 const jwt=require("jsonwebtoken")
 const jwtPassword="secret"
+const zod=require("zod")
+
+
+const emailSchema=zod.string().email()
+const passwordSchema=zod.string().min(6)
 
 
 function signJwt(username,password){
+    const userResponse=emailSchema.safeParse(username)
+    const passwordResponse=passwordSchema.safeParse(password)
+
+    if (!userResponse.success || !passwordResponse.success){
+        return null
+    }
     const signature=jwt.sign({
-        username
+        userResponse
     },jwtPassword)
     return signature
+}
+
+
+function decodeJwt(token){
+    const decoded=jwt.decode(token)
+    if (decoded){
+        return true
+    }else{
+        return false
+    }
+}
+
+function verifyJwt(token){
+    try{
+        jwt.verify(token,jwtPassword)
+        return true
+    }catch (e){
+
+    }
+    return false
 }
 

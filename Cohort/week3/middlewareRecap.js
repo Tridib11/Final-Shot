905f597fe5800bca5fb88c76b1a5a67ccd0faded
Tridib11 <tridibghosh12345@gmail.com>
2024-10-16 01:58:34 +0530

    file added

--- Cohort/week3/middlewareRecap.js
+++ Cohort/week3/middlewareRecap.js
@@ -1,10 +1,26 @@
 const express=require("express");
 const app=express();
 
+
+
+function isOldEnough(age){
+    if (age>=14){
+        return true
+    }else{
+        return false
+    }
+}
 app.get("/ride1",(req,res)=>{
-    res.json({
-        msg:"You have successfully ridden the ride 1"
-    })
+
+    if (isOldEnough(req.query.age)){
+        res.json({
+            msg:"You have successfully ridden the ride 1"
+        })
+    }else{
+        res.json({
+            msg:"Sorry you are not of 14 yet"
+        })
+    }
 })
 
 app.listen(3000)

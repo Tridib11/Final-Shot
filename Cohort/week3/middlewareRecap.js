a3dcf94b7b2857561c58c8f7b1566ae9f8a14131
Tridib11 <tridibghosh12345@gmail.com>
2024-10-16 02:03:24 +0530

    file added

--- Cohort/week3/middlewareRecap.js
+++ Cohort/week3/middlewareRecap.js
@@ -1,26 +1,28 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 
-
-
-function isOldEnough(age){
-    if (age>=14){
-        return true
-    }else{
-        return false
-    }
+function isOldEnough(age) {
+  if (age >= 14) {
+    return true;
+  } else {
+    return false;
+  }
 }
-app.get("/ride1",(req,res)=>{
 
-    if (isOldEnough(req.query.age)){
-        res.json({
-            msg:"You have successfully ridden the ride 1"
-        })
-    }else{
-        res.json({
-            msg:"Sorry you are not of 14 yet"
-        })
-    }
-})
+function useAgeCheckerMiddleware(req, res, next) {
+  const age = req.query.age;
+  if (age >= 14) {
+    next();
+  } else {
+    res.json({
+      msg: "Sorry you are not of 14 yet",
+    });
+  }
+}
+app.get("/ride1", useAgeCheckerMiddleware, (req, res) => {
+  res.json({
+    msg: "You have successfully ridden the ride 1",
+  });
+});
 
-app.listen(3000)
+app.listen(3000);

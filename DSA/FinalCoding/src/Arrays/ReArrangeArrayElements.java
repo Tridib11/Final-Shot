53343410fe5c51edfa877f9e5ea2e128b330cfae
Tridib11 <tridibghosh12345@gmail.com>
2024-10-15 12:55:55 +0530

    File added

--- DSA/FinalCoding/src/Arrays/ReArrangeArrayElements.java
+++ DSA/FinalCoding/src/Arrays/ReArrangeArrayElements.java
@@ -1,9 +1,14 @@
 package Arrays;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ReArrangeArrayElements {
     public static void main(String[] args) {
 
     }
+
+    //variety 1
 
     private static int[] rearrangeArray(int[] arr){
         int n=arr.length;
@@ -20,4 +25,46 @@ public class ReArrangeArrayElements {
         }
         return newArr;
     }
+
+    //variety 2
+
+    public static int[] alternateNumbers(int []arr) {
+        // Write your code here.
+        ArrayList<Integer> positive=new ArrayList<>();
+        ArrayList<Integer> negative=new ArrayList<>();
+        for(int i:arr){
+            if(i<0){
+                negative.add(i);
+            }else{
+                positive.add(i);
+            }
+        }
+        if(positive.size()>negative.size()){
+            for(int i=0;i<negative.size();i++){
+                arr[i*2]=positive.get(i);
+                arr[i*2+1]=negative.get(i);
+            }
+            int filledIndex=negative.size()*2;
+            for(int i=negative.size();i<positive.size();i++){
+                arr[filledIndex]=positive.get(i);
+                filledIndex++;
+            }
+        }
+
+        else{
+            for(int i=0;i<positive.size();i++){
+                arr[i*2]=positive.get(i);
+                arr[i*2+1]=negative.get(i);
+            }
+            int filledIndex=positive.size()*2;
+            for(int i=positive.size();i<negative.size();i++){
+                arr[filledIndex]=negative.get(i);
+                filledIndex++;
+            }
+        }
+
+        return arr;
+    }
+
+
 }

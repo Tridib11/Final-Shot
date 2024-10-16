df3bcfd0dd3dd79217cff8f5ba60053b6505585d
Tridib11 <tridibghosh12345@gmail.com>
2024-10-16 20:29:30 +0530

    File added

---
+++ Cohort/week4/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors=require("cors")
+const app = express();
+
+
+app.use(cors())
+app.get("/sum", (req, res) => {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum=a+b
+  res.send(sum.toString())
+});
+
+app.listen((3000),()=>{
+  console.log("Server started on port 3000")
+})

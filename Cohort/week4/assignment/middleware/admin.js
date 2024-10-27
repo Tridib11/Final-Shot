8d9c46bda3aecdc5ce7c92bba5fc8f1e84a18008
Tridib11 <tridibghosh12345@gmail.com>
2024-10-27 19:51:54 +0530

    file added

--- Cohort/week4/assignment/middleware/admin.js
+++ Cohort/week4/assignment/middleware/admin.js
@@ -1,5 +1,13 @@
-function adminMiddleware(req,res,next){
-
+const {Admin}=require("../db")
+async function adminMiddleware(req,res,next){
+  const username=req.headers.username
+  const password=req.headers.password
+  try{
+    await Admin.findOne({username,password})
+    next()
+  }catch(err){
+    res.status(403).json('User doesnt exists')
+  }
 }
 
 module.exports=adminMiddleware
--- Cohort/week4/assignment/middleware/user.js
+++ Cohort/week4/assignment/middleware/user.js
@@ -1,13 +1,13 @@
-const { Admin } = require("../db");
+const { User } = require("../db");
 
 async function userMiddleware(req, res, next) {
   const username = req.headers.username;
   const password = req.headers.password;
   try {
-    const value = await Admin.findOne({ username, password });
+    const value = await User.findOne({ username, password });
     next();
   } catch (error) {
-    res.status(403).send('User doesnt exits');
+    res.status(403).json('User doesnt exists');
   }
 }
 

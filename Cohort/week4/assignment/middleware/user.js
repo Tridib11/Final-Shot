ed375336ec483187231ef7cbc8332b4a1c2b90e8
Tridib11 <tridibghosh12345@gmail.com>
2024-10-27 19:47:55 +0530

    file added

--- Cohort/week4/assignment/middleware/user.js
+++ Cohort/week4/assignment/middleware/user.js
@@ -8,7 +8,6 @@ async function userMiddleware(req, res, next) {
     const value = await Admin.findOne({ username, password });
     next();
   } catch (error) {
-    // Handle the error appropriately, for example, by sending an error response
     res.status(403).send('User doesnt exits');
   }
 }

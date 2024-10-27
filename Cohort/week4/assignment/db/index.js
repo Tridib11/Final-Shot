14b8babf775c4968004e74123b05e406082ce5e5
Tridib11 <tridibghosh12345@gmail.com>
2024-10-27 19:04:04 +0530

    file added

--- Cohort/week4/assignment/db/index.js
+++ Cohort/week4/assignment/db/index.js
@@ -3,3 +3,27 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI)
         .then(()=>console.log("Database Connected"))
         .catch((err)=>console.error("Database connection error",err))
+
+
+const AdminSchema=new mongoose.Schema({
+
+})
+
+const UserSchema=new mongoose.Schema({
+
+})
+
+const CourseSchema=new mongoose.Schema({
+
+})
+
+const Admin=mongoose.model('Admin',AdminSchema)
+const User=mongoose.model('User',UserSchema)
+const Course=mongoose.model('Course',CourseSchema)
+
+module.exports={
+  Admin,
+  User,
+  Course
+}
+

1d0d7778b9e3cbabb76a38e65a371e528fe888fd
Tridib11 <tridibghosh12345@gmail.com>
2024-10-27 18:47:36 +0530

    Update .gitignore and package.json
    - Add .env to .gitignore
    - Update dependencies in package.json
      - Add dotenv v16.4.5
      - Add mongoose v8.7.3

---
+++ Cohort/week4/assignment/db/index.js
@@ -0,0 +1,5 @@
+const mongoose=require("mongoose")
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI)
+        .then(()=>console.log("Database Connected"))
+        .catch((err)=>console.error("Database connection error",err))

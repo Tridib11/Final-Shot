3b5087b7f8a94084e59768766719ee8bfda81e53
Tridib11 <tridibghosh12345@gmail.com>
2024-10-29 00:58:36 +0530

    Refactor App component to add a counter button with increment functionality

--- Cohort/week5/counter-app/src/App.jsx
+++ Cohort/week5/counter-app/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-function App() {
+
+function App(){
+const[counter,setCounter]=useState(0)
   return (
     <div>
-      Hi there
+      <CustomButton counter={counter} setCounter={setCounter}></CustomButton>
     </div>
   )
+}
+
+function CustomButton(props){
+
+  function onCLickHandler(){
+    props.setCounter(props.counter+1)
+  }
+  return <button onClick={onCLickHandler}>
+    Counter {props.counter}
+  </button>
 }
 
 export default App

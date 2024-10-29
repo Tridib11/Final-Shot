e53d85c399d7ba712ffd71b692acf1c3c8b56d4c
Tridib11 <tridibghosh12345@gmail.com>
2024-10-30 02:19:54 +0530

    Refactor CreateTodo component to add functionality for adding a new todo

--- Cohort/week5/Todo-app/frontend/src/components/CreateTodo.jsx
+++ Cohort/week5/Todo-app/frontend/src/components/CreateTodo.jsx
@@ -7,14 +7,30 @@ function CreateTodo() {
         padding:10,
         margin:10
       }} type="text" placeholder='title' /> <br /><br />
+
+
       <input style={{
         padding:10,
         margin:10
       }} type="text" placeholder='description'/> <br /><br />
+
+
       <button style={{
         padding:10,
         margin:10
-      }} >Add a todo</button>
+      }}  onClick={()=>{
+        fetch("http://localhost:3000/todo",{
+          method:"POST",
+          body:{
+            title:"",
+            description:""
+          }
+        })
+        .then(async (res)=>{
+          const json=await res.json()
+          alert("Todo added")
+        })
+      }}>Add a todo</button>
     </div>
   )
 }

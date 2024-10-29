9f0fb30ec65653ecc3b3889fe27be1924e841c31
Tridib11 <tridibghosh12345@gmail.com>
2024-10-30 01:32:05 +0530

    Refactor App component and add Todos component

---
+++ Cohort/week5/Todo-app/frontend/src/components/Todos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Todos() {
+  return (
+    <div>
+      <h1>Go to the gym</h1>
+      <h2>You need to go to the gym</h2>
+      <button>Mark as completed</button>
+    </div>
+  )
+}

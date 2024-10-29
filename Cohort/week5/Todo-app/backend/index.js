3b0e739b7a1a3f50e36117bc04ef84d46c344fa7
Tridib11 <tridibghosh12345@gmail.com>
2024-10-29 22:13:56 +0530

    File added

--- Cohort/week5/Todo-app/backend/index.js
+++ Cohort/week5/Todo-app/backend/index.js
@@ -1,10 +1,19 @@
 const express=require("express")
+const { createTodo } = require("./types")
 
 const app=express()
 
 app.use(express.json())
 
 app.post("/todo",(req,res)=>{
+  const createPayload=req.body
+  const parsedPayload=createTodo.safeParse(createPayload)
+  if(!parsedPayload.success){
+    res.status(411).json({
+      msg:"You send the wrong inputs"
+    })
+  }
+  //put it in mongodb
 
 })
 

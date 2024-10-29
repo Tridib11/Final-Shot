b009debabc4a1a3a6f2ef9fe65271489437603cf
Tridib11 <tridibghosh12345@gmail.com>
2024-10-30 01:27:03 +0530

    Refactor CSS and remove unused styles

---
+++ Cohort/week5/Todo-app/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CreateTodo() {
+  return (
+    <div>
+      <input type="text" placeholder='title' /> <br /><br />
+      <input type="text" placeholder='description'/> <br /><br />
+    </div>
+  )
+}
+
+export default CreateTodo

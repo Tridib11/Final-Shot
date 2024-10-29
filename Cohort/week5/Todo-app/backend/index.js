739b2ae8f201ef241cfc2fc0f1673107706fe09d
Tridib11 <tridibghosh12345@gmail.com>
2024-10-29 22:16:29 +0530

    Update index.js to include updateTodo function

--- Cohort/week5/Todo-app/backend/index.js
+++ Cohort/week5/Todo-app/backend/index.js
@@ -1,5 +1,5 @@
 const express=require("express")
-const { createTodo } = require("./types")
+const { createTodo, updateTodo } = require("./types")
 
 const app=express()
 
@@ -9,7 +9,7 @@ app.post("/todo",(req,res)=>{
   const createPayload=req.body
   const parsedPayload=createTodo.safeParse(createPayload)
   if(!parsedPayload.success){
-    res.status(411).json({
+    return res.status(411).json({
       msg:"You send the wrong inputs"
     })
   }
@@ -22,7 +22,13 @@ app.get("/todos",(req,res)=>{
 })
 
 app.put("/completed",(req,res)=>{
-
+  const updatePayload=req.body
+  const parsedPayload=updateTodo.safeParse(updatePayload)
+  if(!parsedPayload.success){
+    return res.status(411).json({
+      msg:"You sent the wrong inputs"
+    })
+  }
 })
 
 

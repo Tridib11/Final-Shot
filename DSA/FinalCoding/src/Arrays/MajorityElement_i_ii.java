e169be4f59e117bce9d9a0b34a459aca79859b25
Tridib11 <tridibghosh12345@gmail.com>
2024-10-31 16:15:06 +0530

    File added

--- DSA/FinalCoding/src/Arrays/MajorityElement_i_ii.java
+++ DSA/FinalCoding/src/Arrays/MajorityElement_i_ii.java
@@ -22,6 +22,38 @@ public class MajorityElement_i_ii {
     }
 
     public List<Integer> majorityElement(int[] nums) {
+        int count1=0,count2=0;
+        int element1=Integer.MIN_VALUE;
+        int element2=Integer.MIN_VALUE;
+
+        for(int i=0;i<nums.length;i++){
+            if(count1==0 && nums[i]!=element2){
+                count1=1;
+                element1=nums[i];
+            }else if(count2==0 && nums[i]!=element1){
+                count2=1;
+                element2=nums[i];
+            } else if (nums[i]==element1) {
+                count1++;
+            }else if(nums[i]==element2){
+                count2++;
+            }else{
+                count1--;count2--;
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        count1=0;count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==element1) count1++;
+            if(nums[i]==element2) count2++;
+        }
+
+        int mini=(int) ((nums.length)/3)+1;
+        if(count1>=mini) ans.add(element1);
+        if(count2>=mini) ans.add(element2);
+
+
+        return ans;
 
     }
 }

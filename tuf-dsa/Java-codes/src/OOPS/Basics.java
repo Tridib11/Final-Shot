96afaef5108bd138e04746d201d44d336b7d3995
Tridib11 <tridibghosh12345@gmail.com>
2024-11-01 19:56:18 +0530

    files added

--- tuf-dsa/Java-codes/src/OOPS/Basics.java
+++ tuf-dsa/Java-codes/src/OOPS/Basics.java
@@ -1,6 +1,23 @@
 package OOPS;
 
+class Test{
+    int age;
+    public void assignAge(int num){
+        age=num;
+    }
+}
 
 
 public class Basics {
+    public static void main(String[] args) {
+        Test test1=new Test();
+        test1.assignAge(10);
+
+        Test test2=new Test();
+        test2.assignAge(20);
+
+        System.out.println(test2.age);
+        System.out.println(test1.age);
+
+    }
 }

9c2ec092ed519c8771ae215411c1bd97776afee8
Tridib11 <tridibghosh12345@gmail.com>
2024-11-01 20:39:05 +0530

    files added

---
+++ tuf-dsa/Java-codes/src/OOPS/Inheritance.java
@@ -0,0 +1,44 @@
+package OOPS;
+
+//Base or parent class
+class Vehicle{
+    String numberOfVehicle;
+    public Vehicle(String numberOfVehicle){
+        this.numberOfVehicle=numberOfVehicle;
+    }
+    public void honk(){
+        System.out.println("Honk!!!!!!!!!!!!!");
+    }
+    public void printNumberOfVehicle(){
+        System.out.println(numberOfVehicle);
+    }
+}
+
+//Derived classes
+
+class Car extends Vehicle{
+    public Car(String numberOfCar){
+        super(numberOfCar);
+    }
+}
+
+class Bus extends Vehicle{
+    public Bus(String numberOfBus){
+        super(numberOfBus);
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+
+        Car car=new Car("KA12s3");
+        car.printNumberOfVehicle();
+        car.honk();
+
+        Bus bus=new Bus("WB1234");
+        bus.printNumberOfVehicle();
+        bus.honk();
+
+    }
+
+
+}

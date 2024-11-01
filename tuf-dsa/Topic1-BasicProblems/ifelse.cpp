15ec5be5a84e46f28775ab004786d6ea94e6575c
Tridib11 <tridibghosh12345@gmail.com>
2024-11-01 18:46:25 +0530

    Add ifelse.cpp for age classification and update tasks.json and input/output files

---
+++ tuf-dsa/Topic1-BasicProblems/ifelse.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int age;
+  cout<<"Enter your age "<<endl;
+  cin>>age;
+  if(age>=18){
+    cout<<"adult";
+  }else{
+    cout<<"Kid";
+  }
+  return 0;
+}

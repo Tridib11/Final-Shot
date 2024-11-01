be5049df1c36020a34df2f9ac422bde4289cd2f7
Tridib11 <tridibghosh12345@gmail.com>
2024-11-01 17:05:35 +0530

    Update VSCode settings and modify input/output handling in basic problem files

--- tuf-dsa/Topic1-BasicProblems/input.cpp
+++ tuf-dsa/Topic1-BasicProblems/input.cpp
@@ -1,8 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-  int bdate;
-  cin>>bdate;
-  cout<<bdate;
+  // int gv; // Initialize gv
+  // int bdate;
+  // cin>>bdate;
+  // cout<<bdate<<endl; // Add missing << operator
+  // // cout<<gv;
+
+  string str;
+  cin>>str;
+  string str1;
+  cin>>str1;
+
+  cout<<str<<" "<<str1;
+
+
+
   return 0;
 }

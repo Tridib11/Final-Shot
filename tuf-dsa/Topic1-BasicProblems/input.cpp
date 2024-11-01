d3b333d9fb3b90b9ca36c6912c2b3d02f05a57fa
Tridib11 <tridibghosh12345@gmail.com>
2024-11-01 16:56:11 +0530

    Update VSCode tasks and add new C++ input/output files for basic problem solving

---
+++ tuf-dsa/Topic1-BasicProblems/input.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int bdate;
+  cin>>bdate;
+  cout<<bdate;
+  return 0;
+}
---
+++ tuf-dsa/Topic1-BasicProblems/test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int numInt=INT_MAX;
+  cout<<numInt<<endl;
+
+  long numLong=100000000000L;
+
+  cout<<numLong<<endl;
+
+  long long numlonglong=10000000000LL;
+  cout<<numlonglong;
+  return 0;
+}

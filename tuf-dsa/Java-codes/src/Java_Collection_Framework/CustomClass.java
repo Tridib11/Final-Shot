4fa4bf7256024613685b149daf480d6096678089
Tridib11 <tridibghosh12345@gmail.com>
2024-11-03 15:28:27 +0530

    files added

--- tuf-dsa/Java-codes/src/Java_Collection_Framework/CustomClass.java
+++ tuf-dsa/Java-codes/src/Java_Collection_Framework/CustomClass.java
@@ -2,18 +2,41 @@ package Java_Collection_Framework;
 
 
 class Data{
-    public Integer num;
-    public String name;
-    Data(int _num,String _name){
+    private Integer num;
+    private String name;
+    private InternalData internalData;
+    Data(){
+        System.out.println("Default constructor called");
+    }
+    Data(int _num,String _name,int _revenue){
         this.num=_num;
         this.name=_name;
+        this.internalData=new InternalData(_revenue);
+    }
+
+    public void setNum(Integer _num){
+        this.num=_num;
+    }
+    public void setName(String _name){
+        this.name=_name;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+class InternalData{
+    public Integer revenue;
+    InternalData(Integer _revenue){
+        this.revenue=_revenue;
     }
 }
 public class CustomClass {
     public static void main(String[] args) {
-        Data dataobj1=new Data(9,"Raj");
-        Data dataobj2=new Data(7,"TUF");
-        dataobj1.name="Tridib";
-        System.out.println(dataobj1.name);
+        Data obj=new Data();
+        Data dataobj1=new Data(9,"Raj",10000);
+        Data dataobj2=new Data(7,"TUF",100000);
+        dataobj1.setName("Tridib");
+        System.out.println(dataobj1.getName());
     }
 }

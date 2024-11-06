5d05fb0a8e158af387d88e4402d218241ea69edb
Tridib11 <tridibghosh12345@gmail.com>
2024-11-06 20:18:15 +0530

    add Rectangle class with methods for area calculation and painting

---
+++ Cohort_3/local_Code/week2/Rectangle.js
@@ -0,0 +1,21 @@
+class Rectancle{
+  constructor(width,height,color){
+    this.width=width;
+    this.height=height;
+    this.color=color;
+  }
+
+  area(){
+    const area=this.width*this.height;
+    return area;
+  }
+
+  paint(){
+    console.log(`Painting with color ${this.color}`)
+  }
+}
+
+
+const rect=new Rectancle(2,4,"Red");
+const area=rect.area();
+console.log(area)

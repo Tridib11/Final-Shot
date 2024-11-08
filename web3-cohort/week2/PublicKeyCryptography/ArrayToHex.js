f8e6921a4de5abcab35eb9972debefd8606d6b81
Tridib11 <tridibghosh12345@gmail.com>
2024-11-08 23:32:13 +0530

    add arrayToHex function for converting byte arrays to hexadecimal format

--- web3-cohort/week2/PublicKeyCryptography/ArrayToHex.js
+++ web3-cohort/week2/PublicKeyCryptography/ArrayToHex.js
@@ -1,2 +1,20 @@
-const byteArray=new Uint8Array([72,101,108,108,111])
+function arrayToHex(byteArray){
+  let hexString='';
+  for(let i=0;i<byteArray.length;i++){
+    hexString+=byteArray[i].toString(16).padStart(2,"0");
+    //toString(16) converts it into hexadecimal
+    //
+  }
+  return hexString
+}
+
+
+
+const byteArray=new Uint8Array([72,101,108,108,111]) //corresponds to Hello
+const s="Hello"
+
+const byteArray1= new TextEncoder().encode(s)
+console.log(byteArray1)
 console.log(byteArray)
+
+console.log(arrayToHex(byteArray))

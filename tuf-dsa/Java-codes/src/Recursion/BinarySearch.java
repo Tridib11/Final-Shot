f795181e99a68660cea5bf7a5c40709dd8225aa7
Tridib11 <tridibghosh12345@gmail.com>
2024-11-09 19:45:23 +0530

    files added

---
+++ tuf-dsa/Java-codes/src/Recursion/BinarySearch.java
@@ -0,0 +1,25 @@
+package Recursion;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6,7,8};
+        System.out.println(bs(arr,6,0,arr.length-1));
+
+    }
+
+    static int bs(int[] arr,int target,int start,int end){
+        if(start>end){
+            return -1;
+        }
+
+        int mid=start+(end-start)/2;
+
+        if(arr[mid]==target){
+            return mid;
+        }
+        if(target<arr[mid]){
+            return bs(arr,target,start,mid-1);
+        }
+        return bs(arr,target,mid+1,end);
+    }
+}

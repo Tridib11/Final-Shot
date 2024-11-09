5f9a9694120130ae6155cda395cb11cf46605010
Tridib11 <tridibghosh12345@gmail.com>
2024-11-09 19:45:53 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Recursion/BinarySearch.java
+++ tuf-dsa/Java-codes/src/Recursion/BinarySearch.java
@@ -3,7 +3,7 @@ package Recursion;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6,7,8};
-        System.out.println(bs(arr,6,0,arr.length-1));
+        System.out.println(bs(arr,77,0,arr.length-1));
 
     }
 

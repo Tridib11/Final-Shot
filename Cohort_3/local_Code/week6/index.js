9f1db7f8aa24cbf5d44d4ec6e0e160a8a64e18c9
Tridib11 <tridibghosh12345@gmail.com>
2024-11-13 07:39:48 +0530

    Implement user signup functionality with basic validation

--- Cohort_3/local_Code/week6/index.js
+++ Cohort_3/local_Code/week6/index.js
@@ -6,8 +6,26 @@ const app=express()
 app.use(express.json())
 
 
-app.post('/signup',(req,res)=>{
+const users=[]
 
+app.post('/signup',(req,res)=>{
+  const username=req.body.username
+  const password=req.body.password
+
+
+  if(users.find(u => u.username == username)) {
+    return res.json({
+      msg: "You are already signed up"
+    });
+  }
+  users.push({
+    username:username,
+    password:password
+  })
+
+  res.json({
+    msg:"You are signed in"
+  })
 })
 
 app.post("/signin",(req,res)=>{

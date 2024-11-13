a11f3e81b77f3625eee1f76e4ea78e0c3560b365
Tridib11 <tridibghosh12345@gmail.com>
2024-11-13 07:01:36 +0530

    Refactor fetch function to use async/await and improve data logging

--- Cohort_3/local_Code/week5/fetch.js
+++ Cohort_3/local_Code/week5/fetch.js
@@ -1,7 +1,23 @@
-const func=async()=>{
-  fetch("https://jsonplaceholder.typicode.com/posts/")
-  .then(response=>response.json())
-  .then(data=> console.log(data))
+// const func=async()=>{
+//   fetch("https://jsonplaceholder.typicode.com/posts/")
+//   .then(response=>response.json())
+//   .then(data=> console.log(data))
+// }
+
+function func() {
+  fetch("https://jsonplaceholder.typicode.com/posts/").then(
+    async (response) => {
+      const result = await response.json();
+      // const totalLength=result.length;
+      // for(let i=0;i<totalLength;i++){
+      //   console.log(result[i].id+","+result[i].body)
+      // }
+
+      result.forEach((item)=>{
+        console.log(item.id+","+item.body)
+      })
+    }
+  );
 }
 
-func()
+func();

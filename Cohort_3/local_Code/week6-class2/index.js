afeab41636ac71c54e629f80f52a981acaab5782
Tridib11 <tridibghosh12345@gmail.com>
2024-11-16 23:26:35 +0530

    Implement user signup, signin, and profile retrieval endpoints

--- Cohort_3/local_Code/week6-class2/index.js
+++ Cohort_3/local_Code/week6-class2/index.js
@@ -3,14 +3,62 @@ const jwt=require("jsonwebtoken")
 
 const app=express()
 
-app.post("/signup",(req,res)=>{
+const JWT_SECRET="tridib11"
+const PORT=3000
+app.use(express.json())
 
+
+const users=[]
+app.post("/signup",(req,res)=>{
+  const username=req.body.username
+  const password=req.body.password
+  if(users.find(user => user.username === username)){
+    return res.status(400).json({
+      msg:"User already exists"
+    })
+  }
+  users.push({
+    username:username,
+    password:password
+  })
+
+  res.json({
+    msg:"You are signed in successfully"
+  })
 })
 
-app.post("/signup",(req,res)=>{
-
+app.post("/signin",(req,res)=>{
+  const username=req.body.username
+  const password=req.body.password
+  if(users.find(user=>user.username===username)){
+    const token=jwt.sign({
+      username
+    },JWT_SECRET)
+    res.json({
+      msg:token
+    })
+  }else{
+    res.json({
+      msg:"Sorry user doesnot exists!"
+    })
+  }
 })
 
 app.post("/me",(req,res)=>{
-  
+  const token=req.headers.token
+  const decodedData=jwt.verify(token,JWT_SECRET)
+  const user = users.find(user=>user.username===decodedData.username)
+  if (user) {
+    return res.json({
+      password: user.password
+    })
+  } else {
+    return res.status(404).json({
+      msg: "User not found"
+    })
+  }
+})
+
+app.listen(PORT,()=>{
+  console.log(`Server started at port ${PORT}`)
 })

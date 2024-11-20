b6306e5490e2bcb8b38f1a78a72bad39e956ac18
Tridib11 <tridibghosh12345@gmail.com>
2024-11-21 02:58:11 +0530

    Implement authentication and CRUD operations for todos

--- Cohort_3/local_Code/week7/MONGO/auth.js
+++ Cohort_3/local_Code/week7/MONGO/auth.js
@@ -1,8 +1,9 @@
 const jwt=require("jsonwebtoken")
 const JWT_SECRET="S3Cr3T"
-function auth(req,res,next){
+async function auth(req,res,next){
   const token=req.headers.authorization
-  const response=jwt.verify(token,JWT_SECRET)
+  const response = await jwt.verify(token, JWT_SECRET)
+  console.log("From response middleware:", response.id);
   if(response){
     req.userId=response.id
     next()
--- Cohort_3/local_Code/week7/MONGO/index.js
+++ Cohort_3/local_Code/week7/MONGO/index.js
@@ -1,5 +1,76 @@
 const express=require("express")
 const app=express()
-
+const jwt=require("jsonwebtoken")
+const {auth,JWT_SECRET}=require("./auth")
 const {UserModel,TodoModel}=require("./db")
 
+app.use(express.json())
+
+app.post("/signup",async(req,res)=>{
+  const email=req.body.email
+  const password=req.body.password
+  const name=req.body.name
+  await UserModel.create({
+    email:email,
+    password:password,
+    name:name
+  })
+  res.json({
+    message:"You are signed up"
+  })
+})
+
+app.post("/signin",async(req,res)=>{
+  const email=req.body.email;
+  const password=req.body.password
+  const user=await UserModel.findOne({
+    email:email,
+    password:password
+  })
+  console.log(user)
+
+  if(user){
+    const token=jwt.sign({
+      id:user._id.toString()
+    },JWT_SECRET)
+    res.json({
+      token:token
+    })
+  }else{
+    res.status(403).json({
+      message:"Incorrect Credentials"
+    })
+  }
+})
+
+
+app.post("/todo",auth,async(req,res)=>{
+  const userId=req.userId
+  const title=req.body.title
+  const done=req.body.done
+  await TodoModel.create({
+    userId,
+    title,
+    done
+  })
+
+  res.json({
+    message:"Todo created"
+  })
+})
+
+
+app.get("/todos",auth,async(req,res)=>{
+  const userId=req.userId
+  const todos=await TodoModel.find({
+    userId
+  })
+  res.json({
+    todos
+  })
+})
+
+
+app.listen(3000,()=>{
+  console.log("Server started")
+})

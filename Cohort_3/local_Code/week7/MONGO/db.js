c2933b90c1d9178674965b87d251588a73fe7ef4
Tridib11 <tridibghosh12345@gmail.com>
2024-11-21 00:00:55 +0530

    File added

--- Cohort_3/local_Code/week7/MONGO/db.js
+++ Cohort_3/local_Code/week7/MONGO/db.js
@@ -1,19 +1,32 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.pcgvjbl.mongodb.net/TODO")
+mongoose.connect("mongodb+srv://admin:admin@cluster0.pcgvjbl.mongodb.net/TODO_Cohot_3")
   .then(()=>{
     console.log("DB connected")
   })
 
 
+const Schema=mongoose.Schema
+const ObjectId=Schema.ObjectId
 
-const User=mongoose.Schema({
+const User=new Schema({
   email:String,
   password:String,
   name:String
 })
 
-const Todo=mongoose.Schema({
+const Todo=new Schema({
   title:String,
-  done:Boolean
+  done:Boolean,
+  userId:ObjectId
 })
+
+
+const UserModel=mongoose.model('users',User)
+const TodoModel=mongoose.model('todos',Todo)
+
+
+module.exports={
+  UserModel,
+  TodoModel
+}

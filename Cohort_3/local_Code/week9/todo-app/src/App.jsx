5c502a5e9db1fd2061fb8922b4c4d35ecde54770
Tridib11 <tridibghosh12345@gmail.com>
2024-11-23 08:56:29 +0530

    Files added1

---
+++ Cohort_3/local_Code/week9/todo-app/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const[todos,setTodos]=useState([])
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return(
+    <div>
+      <input
+        type="text"
+        value={title}
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)} // Update title state
+      />
+      <br />
+      <input
+        type="text"
+        value={description}
+        placeholder="Description"
+        onChange={(e) => setDescription(e.target.value)} // Update description state
+      />
+      <br />
+
+      <button
+        onClick={() => {
+          if (title && description) {
+            // Add new todo
+            setTodos([
+              ...todos,
+              {
+                id: todos.length + 1, // Generate unique ID
+                title,
+                description,
+                done: false,
+              },
+            ]);
+            setTitle(""); // Clear title input
+            setDescription(""); // Clear description input
+          }
+        }}
+      >
+        Add todo
+      </button>
+
+      <br />
+
+      {/* {todos.map((todo)=>(
+        <Todo title={todo.title}   description={todo.description}/>
+      ))} */}
+      
+      {/*or*/}
+
+      {todos.map((todo)=>{
+        return <Todo key={todo.id}  title={todo.title}   description={todo.description}/>
+      })}
+    </div>
+  )
+}
+
+
+
+function Todo(props){
+  return <div>
+    <h1>{props.title}</h1>
+    <h3>{props.description}</h3>
+  </div>
+}
+
+export default App

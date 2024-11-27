341c79d79efc7774eb724d42c374a7b04c6db338
Tridib11 <tridibghosh12345@gmail.com>
2024-11-27 09:41:06 +0530

    Add FindTodo component to fetch and display todos by ID

--- Cohort/week6.2/src/FindTodo.jsx
+++ Cohort/week6.2/src/FindTodo.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
-function App() {
+function FindTodo() {
   const[todos,setTodos]=useState([])
+  const[id,setId]=useState(0)
   
   useEffect(()=>{
-    axios.get("https://jsonplaceholder.typicode.com/todos")
+    axios.get(`https://jsonplaceholder.typicode.com/todos?id=${id}`)
          .then(response =>{
           setTodos(response.data)
          })
-  },[])
+  },[id])
   return (
     <div>
       {todos.map(todo=>{
         return <DisplayTodo key={todo.id} title={todo.title} description={todo.completed? "Completed" :"Pending"} />
       })}
+      <TextBox setId={setId}/>
     </div>
   )
+}
+
+
+function TextBox({setId}){
+  return <div>
+    <input type="text" placeholder='id'  onChange={(e)=>{
+      setId(e.target.value)
+    }} />
+  </div>
 }
 
 function DisplayTodo({title,description}){
@@ -25,5 +36,4 @@ function DisplayTodo({title,description}){
     <h3>{description}</h3>
   </div>
 }
-
-export default App
+export default FindTodo

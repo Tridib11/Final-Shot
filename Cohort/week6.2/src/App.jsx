5935b23a60865b9c945dc0962fc8c9b664ece710
Tridib11 <tridibghosh12345@gmail.com>
2024-11-27 09:25:17 +0530

    Add axios dependency and implement API call to fetch todos in App component

--- Cohort/week6.2/src/App.jsx
+++ Cohort/week6.2/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
-
+import axios from 'axios'
 function App() {
-  const [count, setCount] = useState(0)
-
+  const[todos,setTodos]=useState([])
+  useEffect(()=>{
+    axios.get("https://jsonplaceholder.typicode.com/todos")
+         .then(response =>{
+          setTodos(response.data)
+         })
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {todos.map(todo=>{
+        return <DisplayTodo key={todo.id} title={todo.title} description={todo.completed? "Completed" :"Pending"} />
+      })}
+    </div>
   )
+}
+
+function DisplayTodo({title,description}){
+  return <div>
+    <h1>{title}</h1>
+    <h3>{description}</h3>
+  </div>
 }
 
 export default App

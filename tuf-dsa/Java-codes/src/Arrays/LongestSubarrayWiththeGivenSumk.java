279919c8d0598e29f660faa2f5a74d33067ed752
Tridib11 <tridibghosh12345@gmail.com>
2024-11-28 10:17:52 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Arrays/LongestSubarrayWiththeGivenSumk.java
+++ tuf-dsa/Java-codes/src/Arrays/LongestSubarrayWiththeGivenSumk.java
@@ -1,2 +1,31 @@
-package Arrays;public class LongestSubarrayWiththeGivenSumk {
+package Arrays;
+import java.util.*;
+
+public class LongestSubarrayWiththeGivenSumk {
+    public static void main(String[] args) {
+
+    }
+
+    public int longestSubarray(int[] nums, int sum) {
+        int start=0;
+        int end=-1;
+        int currentSum=0;
+        int longest=Integer.MIN_VALUE;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            currentSum+=nums[i];
+            if(currentSum-sum==0){
+                start=0;
+                end=i;
+                break;
+            }
+            if(map.containsKey(currentSum-sum)){
+                start=map.get(currentSum-sum)+1;
+                end=i;
+                longest=Math.max(longest,(end-start)+1);
+            }
+            map.put(currentSum,i);
+        }
+        return longest;
+    }
 }
--- tuf-dsa/Java-codes/src/Arrays/Maximum_Product_subarray.java
+++ tuf-dsa/Java-codes/src/Arrays/Maximum_Product_subarray.java
@@ -1,2 +1,24 @@
-package Arrays;public class Maximum_Product_subarray {
+package Arrays;
+
+public class Maximum_Product_subarray {
+    public static void main(String[] args) {
+
+    }
+    public int maxProduct(int[] nums) {
+        int leftProduct=1;
+        int rightProduct=1;
+        int ans=nums[0];
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            leftProduct=leftProduct==0?1:leftProduct;
+            rightProduct=rightProduct==0?1:rightProduct;
+
+            leftProduct*=nums[i];
+            rightProduct*=nums[n-i-1];
+
+            ans=Math.max(ans,Math.max(leftProduct,rightProduct));
+        }
+
+        return ans;
+    }
 }

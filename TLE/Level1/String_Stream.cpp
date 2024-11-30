79945191ae17230f9c7555375a73a8c34ced8440
Tridib11 <tridibghosh12345@gmail.com>
2024-11-30 21:49:11 +0530

    C++ files added

--- TLE/Level1/String_Stream.cpp
+++ TLE/Level1/String_Stream.cpp
@@ -1,5 +1,26 @@
 //
 // Created by tridi on 30-11-2024.
 //
+#include<iostream>
+#include <sstream>
+using namespace std;
+int main(){
+  string s1,s2;
+  getline(cin,s1);
+  getline(cin,s2);
+  stringstream ss1(s1),ss2(s2);
+
+  string f1,l1,f2,l2;
+  ss1>>f1>>l1;
+  ss2>>f2>>l2;
+
+  if (l1==l2) {
+    cout<<"ARE Brothers"<<endl;
+  }else {
+    cout<<"NOT"<<endl;
+  }
+
+
+}
 
 

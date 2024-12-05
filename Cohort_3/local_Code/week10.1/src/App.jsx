b6aad769f2d92a0288b482964f8a7968643e0073
Tridib11 <tridibghosh12345@gmail.com>
2024-12-05 21:55:58 +0530

    Add NotfoundPage component and update routing in App component

--- Cohort_3/local_Code/week10.1/src/App.jsx
+++ Cohort_3/local_Code/week10.1/src/App.jsx
@@ -1,16 +1,28 @@
-import "./App.css"
+
 import React from 'react'
 import {BrowserRouter,Route,Routes} from "react-router-dom"
 function App() {
   return (
-    <div>
+    <div style={style}>
       <BrowserRouter>
         <Routes>
           <Route path='/class11' element={<Class11program/>}/>
+          <Route path='*' element={<NotfoundPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
   )
+}
+
+
+const style={display:"flex", justifyContent:"center", alignItems:"center",height:"100vh"}
+
+function NotfoundPage(){
+  return <div>
+    <div style={style}>
+      <h1>Sorry page doesn't exists</h1>
+    </div>
+  </div>
 }
 
 function Class11program(){

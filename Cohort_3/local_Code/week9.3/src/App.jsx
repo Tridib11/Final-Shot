6ea644e173f42b0e88d40523e647ae6febd86f92
Tridib11 <tridibghosh12345@gmail.com>
2024-12-05 12:41:54 +0530

    Refactor App component and remove unused CSS file

--- Cohort_3/local_Code/week9.3/src/App.jsx
+++ Cohort_3/local_Code/week9.3/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div style={{background:"#dfe6e9",height:"100vh"}}>
+      <div style={{display:"flex",justifyContent:"center"}}>
+        <div>
+        <PostComponent />
+        <PostComponent />
+        <PostComponent />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const style = {
+  width: 200,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderColor: "gray",
+  borderWidth: 1,
+  padding:20,
+  marginTop:10
+};
+
+function PostComponent() {
+  return (
+    <div style={style}>
+      <div style={{display:"flex"}}>
+        <img
+          src={
+            "https://media.istockphoto.com/id/2165724939/photo/girl-with-her-hamster-smiling-at-home.jpg?s=2048x2048&w=is&k=20&c=V8dtlrS7Cljtx646RQ-w9RrZGardO7SqIjLWCoL86Xc="
+          }
+          style={{
+            width: 30,
+            height: 20,
+            borderRadius: 20,
+          }}
+        />
+        <div style={{fontSize:10,marginLeft:10}}>
+          <b>100xdevs</b>
+          <div>23,888 followers</div>
+          <div>12m</div>
+        </div>
+      </div>
+      <div style={{fontSize:12}}>want to know how to win big? check this out of $6000 bounties</div>
+    </div>
+  );
+}
+
+export default App;

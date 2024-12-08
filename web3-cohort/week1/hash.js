9faa353f3728db90e10f647c81ba60f8eb610ded
Tridib Ghosh <86187166+Tridib11@users.noreply.github.com>
2024-12-09 01:06:45 +0530

    Delete web3-cohort/week1 directory

--- web3-cohort/week1/hash.js
+++
@@ -1,6 +0,0 @@
-const crypto=require("crypto")
-const input="100xdevs"
-const hash=crypto.createHash('sha256').update(input).digest('hex')
-console.log(hash)
-
-//55711980d50fd24989409a7a7d4e15fcd206556eec1245c94a6e60910dc4e41d

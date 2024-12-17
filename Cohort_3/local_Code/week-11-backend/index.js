4ed369d77a38f852cce6f1896e1dfc255fa4169f
Tridib11 <tridibghosh12345@gmail.com>
2024-12-17 20:26:53 +0530

    Add endpoint to fetch todo by ID and implement todo atom family for Recoil state management

--- Cohort_3/local_Code/week-11-backend/index.js
+++ Cohort_3/local_Code/week-11-backend/index.js
@@ -6,15 +6,39 @@ const app = express();
 
 app.use(cors());
 
-app.get("/todos", (req, res) => {
-  try {
-    const randomNumber = Math.floor(Math.random() * 9) + 1;
-    const ans = TODOS.find((x) => x.id === randomNumber);
+app.get("/todos/:id", (req, res) => {
+  try {    
+    const todoId = parseInt(req.query.id);
+    console.log(todoId)
+    const ans = TODOS.find((x) => x.id === todoId);
     res.json(ans);
   } catch (e) {
     console.log(e);
   }
 });
+
+app.get("/todos", (req, res) => {
+  try {
+    const todoId = parseInt(req.query.id);
+    console.log(todoId);
+    const ans = TODOS.find((x) => x.id === todoId);
+    res.json(ans);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// app.get("/todos", (req, res) => {
+//   try {
+//     const randomNumber = Math.floor(Math.random() * 9) + 1;
+//     const ans = TODOS.find((x) => x.id === randomNumber);
+//     res.json(ans);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
+
 
 app.listen(3000, () => {
   console.log("Server started ");

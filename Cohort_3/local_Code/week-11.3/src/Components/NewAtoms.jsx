1fa83370ef7860668a03a27d231e689525672160
Tridib11 <tridibghosh12345@gmail.com>
2024-12-17 13:32:06 +0530

    Refactor notification atoms and selectors for improved structure; update Testing component to include RecoilRoot and display notification counts

--- Cohort_3/local_Code/week-11.3/src/Components/NewAtoms.jsx
+++ Cohort_3/local_Code/week-11.3/src/Components/NewAtoms.jsx
@@ -1,19 +1,24 @@
-import { atom, selector } from "recoil";
+  import { atom, selector } from "recoil";
 
-export const notifications = atom({
-  key: "networkAtom",
-  default: {},
-});
+  export const notifications = atom({
+    key: "testingAtom",
+    default: {
+        network:0,
+        jobs:0,
+        messaging:0,
+        notifications:0
+    },
+  });
 
-export const totalNotificationSelector = selector({
-  key: "totalNotificationCount",
-  get: ({ get }) => {
-    const allNotifications = get(notifications);
-    return (
-      allNotifications.jobs +
-      allNotifications.network +
-      allNotifications.notification +
-      allNotifications.messaging
-    );
-  },
-});
+  export const totalNotificationSelector = selector({
+    key: "testingTotalNotificationCount",
+    get: ({ get }) => {
+      const allNotifications = get(notifications);
+      return (
+        allNotifications.jobs +
+        allNotifications.network +
+        allNotifications.notifications +
+        allNotifications.messaging
+      );
+    },
+  });
--- Cohort_3/local_Code/week-11.3/src/Testing.jsx
+++ Cohort_3/local_Code/week-11.3/src/Testing.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react'
-import { useRecoilState, useRecoilValue } from 'recoil'
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
 import { notifications, totalNotificationSelector } from './Components/NewAtoms'
 import axios from 'axios'
 
 function Testing() {
   return (
     <div>
-      
+      <RecoilRoot>
+        <MainApp/>
+      </RecoilRoot>
     </div>
   )
 }
 
 
 function MainApp(){
-  const [networkCount,setNetworkCount]=useRecoilState(notifications)
+  const [networkCount, setNetworkCount] = useRecoilState(notifications);
   const totalNotificationCount=useRecoilValue(totalNotificationSelector)
   useEffect(()=>{
     axios.get("http://localhost:3000/notification")
@@ -21,6 +23,16 @@ function MainApp(){
       setNetworkCount(res.data)
     })
   },[])
+
+  return <div>
+    <button>Home</button>
+    <button>My networks ({networkCount.network})</button>
+    <button>Jobs ({networkCount.jobs})</button>
+    <button>Messaging ({networkCount.messaging})</button>
+    <button>Notifications ({networkCount.notifications})</button>
+    
+    <button>Me ({totalNotificationCount})</button>
+  </div>
 }
 
 export default Testing

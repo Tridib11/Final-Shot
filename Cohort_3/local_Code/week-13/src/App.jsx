d38da69366d9d89f2dfdb6fe5a22dcc1c7277afd
Tridib11 <tridibghosh12345@gmail.com>
2024-12-20 11:56:18 +0530

    Refactor App component layout and add Project1 component; update Tailwind CSS configuration

--- Cohort_3/local_Code/week-13/src/App.jsx
+++ Cohort_3/local_Code/week-13/src/App.jsx
@@ -12,10 +12,15 @@ function App() {
         <div className='bg-orange-100'>Class 2</div>
         <div className='bg-lime-500'>Class 3</div>
       </div> */}
-      <div className='grid grid-cols-12'>
+      {/* <div className='grid grid-cols-12'>
         <div className='md:bg-green-700 sm:bg-blue-600 col-span-5 bg-purple-600  '>Class 1</div>
         <div className='bg-red-400 col-span-5'>Class 2</div>
         <div className='bg-green-500 col-span-2'>Class 3</div>
+      </div> */}
+      <div className='grid grid-cols-12'>
+        <div className='col-span-12 sm:col-span-5 bg-purple-600 '>Class 1</div>
+        <div className='col-span-12 sm:col-span-5 bg-red-400 '>Class 2</div>
+        <div className='col-span-12 sm:col-span-2 bg-green-500 '>Class 3</div>
       </div>
     </>
   )
--- Cohort_3/local_Code/week-13/src/main.jsx
+++ Cohort_3/local_Code/week-13/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Project1 from './Project1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Project1/>
   </StrictMode>,
 )

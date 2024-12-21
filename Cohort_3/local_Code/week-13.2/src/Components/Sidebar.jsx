334f38534bc6f1eb4f9035f5641395921c779295
Tridib11 <tridibghosh12345@gmail.com>
2024-12-21 23:21:27 +0530

    Add Tailwind CSS configuration and Sidebar component; refactor App component

---
+++ Cohort_3/local_Code/week-13.2/src/Components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export function Sidebar(){
+  return <div className="flex">
+    <div className="bg-red-200">
+      Sidebar
+    </div>
+    <div className="bg-green-800">
+      Content
+    </div>
+
+  </div>
+}

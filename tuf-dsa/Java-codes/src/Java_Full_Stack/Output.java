b65e20e6eeaef79729ddb4f8d348daa54e52bd12
Tridib11 <tridibghosh12345@gmail.com>
2025-01-03 01:21:00 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Output.java
@@ -0,0 +1,11 @@
+package Java_Full_Stack;
+
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        System.out.println("Kunal");
+        System.out.println(new int[]{1,2,3,4});
+        String name=null;
+        System.out.println(name);
+    }
+}

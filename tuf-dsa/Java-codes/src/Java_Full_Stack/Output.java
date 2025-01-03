a61932d19b1cc932c74505347ba2661595f28f74
Tridib11 <tridibghosh12345@gmail.com>
2025-01-03 19:04:14 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Output.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Output.java
@@ -1,13 +1,17 @@
 package Java_Full_Stack;
-
+import java.util.*;
 public class Output {
     public static void main(String[] args) {
         System.out.println(56);
         System.out.println("Kunal");
-        System.out.println(new int[]{1,2,3,4});
+        System.out.println(Arrays.toString(new int[]{1,2,3,4}));
         String name=null;
         System.out.println(name);
         System.out.println("a"+1);
         System.out.println('a'+1);
+
+
+        String a=new String("RAm");
+        System.out.println(a);
     }
 }

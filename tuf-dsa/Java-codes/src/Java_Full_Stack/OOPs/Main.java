a50d19c8689803da2a78da7b8525f267d1b1389d
Tridib11 <tridibghosh12345@gmail.com>
2025-01-05 23:53:25 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/Main.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/Main.java
@@ -3,8 +3,8 @@ package Java_Full_Stack.OOPs;
 public class Main {
     public static void main(String[] args) {
         Student Tridib=new Student();
-
-
+        System.out.println(Tridib);
+        System.out.println(Tridib.rno);
     }
 }
 
@@ -12,6 +12,4 @@ class Student{
     int rno;
     String name;
     float marks;
-
-
 }

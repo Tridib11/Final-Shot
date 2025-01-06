942a9d508eba40645ea5f6837ee8c6cfd8ad294c
Tridib11 <tridibghosh12345@gmail.com>
2025-01-06 23:11:14 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InstanceVariable.java
@@ -0,0 +1,15 @@
+package Java_Full_Stack.OOPs;
+
+public class InstanceVariable {
+
+
+    int a=10; //Instance
+    int b;
+    //variables that are declared inside the class and outside the method are called
+    //instance variable
+    public static void main(String[] args) {
+        InstanceVariable instance=new InstanceVariable();
+        System.out.println(instance.a);
+        System.out.println(instance.b);
+    }
+}

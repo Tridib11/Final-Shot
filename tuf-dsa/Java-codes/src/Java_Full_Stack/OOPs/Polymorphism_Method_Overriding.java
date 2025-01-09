906b5446e09c20ff69735e863e2d455139ecce02
Tridib11 <tridibghosh12345@gmail.com>
2025-01-09 23:32:49 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/Polymorphism_Method_Overriding.java
@@ -0,0 +1,25 @@
+package Java_Full_Stack.OOPs;
+/*
+Method Overiding (Happens at run time )
+
+When a derived class Extends a parent class and we use the derived class Object then
+the function of the derived class is only called
+ */
+public class Polymorphism_Method_Overriding {
+    public static void main(String[] args) {
+        Deer d=new Deer();
+        d.eat();
+    }
+}
+
+class Animalss{
+    void eat(){
+        System.out.println("Eats anything");
+    }
+}
+
+class Deer extends Animalss{
+    void eat(){
+        System.out.println("Eats Grass");
+    }
+}

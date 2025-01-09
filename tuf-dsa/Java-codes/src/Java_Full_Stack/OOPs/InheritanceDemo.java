e9df99f057c715568bf79dc3f8e131d554a77e09
Tridib11 <tridibghosh12345@gmail.com>
2025-01-09 23:05:01 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InheritanceDemo.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InheritanceDemo.java
@@ -1,4 +1,33 @@
 package Java_Full_Stack.OOPs;
 
+
+/*
+* Inheritance is when properties and methods of base class are passed to a derived class
+* */
 public class InheritanceDemo {
+    public static void main(String[] args) {
+        Fish shark=new Fish();
+        shark.eat();
+    }
+}
+//Base Class
+class Animal{
+    String color;
+
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breath(){
+        System.out.println("Breathes");
+    }
+}
+
+
+//Derived class
+class Fish extends Animal{
+    int fins;
+    void swim(){
+        System.out.println("Swims in water");
+    }
 }

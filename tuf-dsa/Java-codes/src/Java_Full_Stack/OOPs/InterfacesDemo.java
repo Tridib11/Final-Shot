55465c8c736e8ddef651b800467e58aedcb21b3f
Tridib11 <tridibghosh12345@gmail.com>
2025-01-10 15:55:56 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InterfacesDemo.java
@@ -0,0 +1,17 @@
+package Java_Full_Stack.OOPs;
+/*
+
+Interface - Blueprint of the class
+
+- All methods are public,abstract and without implementation
+- Used to achieve total abstraction
+- Variables in the interface are final,public and Static
+
+Multiple Inheritance
+
+To acheive 100% abstraction
+
+ */
+public class InterfacesDemo {
+
+}

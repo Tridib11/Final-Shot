4dcb4a1f2b704e094a5212f77c63dd38528af840
Tridib11 <tridibghosh12345@gmail.com>
2025-01-11 02:16:35 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/Polymorphism_Method_Overriding.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/Polymorphism_Method_Overriding.java
@@ -5,6 +5,8 @@ Method Overiding (Happens at run time )
 When a derived class Extends a parent class and we use the derived class(child class)
 Object then
 the function of the derived class is only called
+
+same method name , same method parameter same return type
  */
 public class Polymorphism_Method_Overriding {
     public static void main(String[] args) {

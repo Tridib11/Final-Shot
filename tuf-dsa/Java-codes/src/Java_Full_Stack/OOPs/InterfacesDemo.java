67252449c4a1bc2a204463538789586af08a3c71
Tridib11 <tridibghosh12345@gmail.com>
2025-01-10 16:03:22 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InterfacesDemo.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPs/InterfacesDemo.java
@@ -1,7 +1,7 @@
 package Java_Full_Stack.OOPs;
 /*
 
-Interface - Blueprint of the class
+Interface - Blueprint of the class used by the (Implements) keyword
 
 - All methods are public,abstract and without implementation
 - Used to achieve total abstraction
@@ -13,5 +13,30 @@ To acheive 100% abstraction
 
  */
 public class InterfacesDemo {
+    public static void main(String[] args) {
+        Queen q=new Queen();
+        q.moves();
+    }
+}
 
+interface chessPlayer{
+    void moves();
+}
+
+class Queen implements chessPlayer{
+    public void moves(){
+        System.out.println("up,down,left,right, diagonal in all 4 directions");
+    }
+}
+
+class Rook implements chessPlayer{
+    public void moves(){
+        System.out.println("up,down,left,right");
+    }
+}
+
+class King implements chessPlayer{
+    public void moves(){
+        System.out.println("Moves by 1 place in all direction");
+    }
 }

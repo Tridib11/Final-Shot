e7bf28443d907db823007876e3c9d80261274884
Tridib11 <tridibghosh12345@gmail.com>
2025-01-14 08:23:42 +0530

    Enhance useTodos hook to support polling with interval parameter and improve loading state management

--- Cohort_3/Custom-hooks/src/DataFecthingHook.jsx
+++ Cohort_3/Custom-hooks/src/DataFecthingHook.jsx
@@ -1,34 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function useTodos(){
-  const[todos,setTodos]=useState([])
-  const[loading,setLoading]=useState(true)
-  useEffect(()=>{
-    axios.get("https://final-shot.onrender.com/todos")
-    .then(res=>{
-      setTodos(res.data.todos)
-      setLoading(false)
-    })
-  },[])
+function useTodos(n) {
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      axios.get("https://final-shot.onrender.com/todos").then((res) => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      });
+    }, n * 1000);
 
-  return {todos,loading}
+    axios.get("https://final-shot.onrender.com/todos").then((res) => {
+      setTodos(res.data.todos);
+      setLoading(false);
+    });
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [n]);
+
+  return { todos, loading };
 }
 function DataFecthingHook() {
-  const {todos,loading}=useTodos()
+  const { todos, loading } = useTodos(5);
   return (
     <div>
-      {loading?"Loading....":todos.map(todo=><Track key={todo.id} todo={todo}/>)}
+      {loading
+        ? "Loading...."
+        : todos.map((todo) => <Track key={todo.id} todo={todo} />)}
     </div>
-  )
+  );
 }
 
-function Track({todo}){
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
+function Track({ todo }) {
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  );
 }
 
-export default DataFecthingHook
+export default DataFecthingHook;

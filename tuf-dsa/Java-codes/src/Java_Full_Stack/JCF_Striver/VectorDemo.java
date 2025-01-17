72bfea998858f9c7f034cf3481085688c56e7601
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 06:39:18 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/VectorDemo.java
@@ -0,0 +1,14 @@
+package Java_Full_Stack.JCF_Striver;
+
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        /*
+        Thread safe
+         */
+
+        Vector<Integer> vec=new Vector<>();
+
+    }
+}

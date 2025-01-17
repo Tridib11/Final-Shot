26e4d375c2898d05d93a6c767241876583c480c2
Tridib11 <tridibghosh12345@gmail.com>
2025-01-18 00:09:58 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/CustomComparatorDemo.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/CustomComparatorDemo.java
@@ -2,6 +2,7 @@ package Java_Full_Stack.JCF_Striver;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class CustomComparatorDemo {
     public static void main(String[] args) {
@@ -12,6 +13,27 @@ public class CustomComparatorDemo {
 
         System.out.println(ans);
         Collections.sort(ans);
+        System.out.println(ans);
+
+        //Sorted in a descending order
+        Collections.sort(ans, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer num1, Integer num2) {
+                //num1<num2
+                //order is wrong
+                /*
+                If not in the correct order then return Positive 1
+                if it is in the correct order return -1 else 0
+                 */
+                if(num1<num2){
+                    return 1;
+                }else if(num1>num2){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            }
+        });
         System.out.println(ans);
     }
 }

d5029bc42343f63c7012b97d916c970b40d65160
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 17:40:54 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/HashMapDemo.java
@@ -0,0 +1,23 @@
+package Java_Full_Stack.JCF_Striver;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        /*
+        Not a neceseity it might not be in a sorted order
+        if order doesnot matter use hashMap
+
+        all functions use O(1)
+         */
+        HashMap<Integer,String> map=new HashMap<>();
+        map.put(1,"Tridib");
+        map.put(2,"Raj");
+        map.put(3,"Rima");
+        System.out.println(map);
+        System.out.println(map.get(3));
+        System.out.println(map.size());
+        map.remove(2);
+        System.out.println(map);
+    }
+}

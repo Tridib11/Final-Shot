a5744d9878e12f03b9949fb51378aae19cad137d
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 06:26:56 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/StackDemo.java
@@ -0,0 +1,21 @@
+package Java_Full_Stack.JCF_Striver;
+
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+        /*
+        LIFO
+         */
+
+        Stack<Integer> s=new Stack<>();
+        s.push(2);
+        s.push(4);
+        s.push(6);
+
+        System.out.println(s);
+        System.out.println(s.peek());
+        s.pop();
+        System.out.println(s.peek());
+    }
+}

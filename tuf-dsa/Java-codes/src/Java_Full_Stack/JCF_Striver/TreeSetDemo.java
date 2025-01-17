1d41a774a605238bcef36eee14cad4b76d503a81
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 06:46:36 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/TreeSetDemo.java
@@ -0,0 +1,26 @@
+package Java_Full_Stack.JCF_Striver;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        /*
+         * Stores unique element in any Sorted order
+         *
+         * */
+
+        TreeSet<Integer> ts=new TreeSet<>();
+        ts.add(1);
+        ts.add(1);
+        ts.add(2);
+        ts.add(4);
+        ts.add(-1);
+
+        System.out.println(ts);
+        for(var i:ts){
+            System.out.println(i);
+        }
+        ts.remove(2);
+        System.out.println(ts);
+    }
+}

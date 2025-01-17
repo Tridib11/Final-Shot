a68bbbe75e594010c947af25d2e7967a5864dd53
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 17:08:45 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/PriorityQueueDemo.java
@@ -0,0 +1,14 @@
+package Java_Full_Stack.JCF_Striver;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+    public static void main(String[] args) {
+        /*
+        Min heap ds
+         */
+
+        PriorityQueue<Integer> pq=new PriorityQueue();
+
+    }
+}

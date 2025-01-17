ea3b116272096a269032f65a8105d14fc4319b51
Tridib11 <tridibghosh12345@gmail.com>
2025-01-17 17:16:33 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/PriorityQueueDemo.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Striver/PriorityQueueDemo.java
@@ -6,9 +6,34 @@ public class PriorityQueueDemo {
     public static void main(String[] args) {
         /*
         Min heap ds
+        You will not be able to see the ascending order but
+        Stores element and whenever you ask for peek it will give you the smallest element
          */
 
         PriorityQueue<Integer> pq=new PriorityQueue();
+        pq.offer(1);
+        pq.offer(0);
+        pq.offer(5);
+        pq.offer(4);
+
+
+//        while(pq.isEmpty()==false){
+//            System.out.print(pq.peek()+",");
+//            pq.poll();
+//        }
+//        System.out.println();
+
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        pq.poll();
+        System.out.println(pq);
+        System.out.println(pq.peek());
+
+        pq.poll();
+        System.out.println(pq);
+        pq.peek();
+
+
 
     }
 }

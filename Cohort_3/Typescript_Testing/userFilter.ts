dd3523b54eb1448ab184936bab8f967a99f5f312
Tridib11 <tridibghosh12345@gmail.com>
2025-01-22 05:26:37 +0530

    Add user filtering function to return users aged 18 and above

---
+++ Cohort_3/Typescript_Testing/userFilter.ts
@@ -0,0 +1,30 @@
+interface Users2{
+  firstName:string,
+  lastName:string,
+  age:number
+}
+
+
+function filteredUsers(users:Users2[]){
+  return users.filter(x=>x.age>=18)
+}
+
+
+const details:Users2[]=[
+  {
+    firstName:"Tridib",
+    lastName:"Ghosh",
+    age:21
+  },
+  {
+    firstName:"Rohan",
+    lastName:"Kaur",
+    age:22
+  },
+  {
+    firstName:"Omraj",
+    lastName:"Kamat",
+    age:11
+  }
+]
+console.log(filteredUsers(details))

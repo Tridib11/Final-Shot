dd2402ab649d136ab9cf5f1537628f5d920199ab
Tridib11 <tridibghosh12345@gmail.com>
2025-01-22 09:39:15 +0530

    Add TypeScript examples for generic functions and enums

---
+++ Cohort_3/Typescript_Testing/GenericTesting.ts
@@ -0,0 +1,8 @@
+type Input=number | string
+
+function firstEl(arr:Input[]){
+  return arr[0]
+}
+
+const value1=firstEl(["Tridib","Rohan"])
+console.log(value1)
--- Cohort_3/Typescript_Testing/enums.ts
+++ Cohort_3/Typescript_Testing/enums.ts
@@ -1,9 +1,40 @@
-type keyInput="up"|"down"|"left"|"right";
-function doSomething(keyPressed:keyInput){
-  if(keyPressed=="up"){
-    
+// type keyInput="up"|"down"|"left"|"right";
+
+/*
+Enums (short for enumerations) in TypeScript are a feature that allows you to define a set of named constants.
+The concept behind an enumeration is to create a human-readable way to represent a set of constant values,r.'hich might otherwise be represented as numbers or strings.
+*/
+
+// enum Direction{
+//   up, //0
+//   down, //1
+//   left, //2
+//   right //3
+// }
+
+
+//to change the enums in compile time 
+
+enum Direction{
+  up ="up", 
+  down = "down", 
+  left = "left", 
+  right ="right" 
+}
+function doSomething(keyPressed:Direction){
+  if(keyPressed==Direction.up){
+    console.log("pressed up")
+  }
+  else if(keyPressed==Direction.down){
+    console.log("Pressed down")
   }
 }
 
-doSomething("up")
-doSomething("down")
+doSomething(Direction.up)
+doSomething(Direction.down)
+
+
+console.log(Direction.up)//0
+console.log(Direction.down)//1
+console.log(Direction.left)//2
+console.log(Direction.right)//3

81fb935633ecd29d5b72ae47570b023445d23741
Tridib11 <tridibghosh12345@gmail.com>
2025-01-23 02:48:22 +0530

    Add new TypeScript interfaces for User and People with example implementations

---
+++ Cohort_3/TypeScript-Testing-2/Interface2.ts
@@ -0,0 +1,24 @@
+// interface Address{
+//   city:string,
+//   country:string,
+//   pincode:number,
+//   houseNumber:number
+// }
+
+// interface User1{
+//   name:string,
+//   age:number,
+//   address:Address
+// }
+
+// interface Offcie{
+//   address:Address
+// }
+
+// let user2:User={
+//   name:"Tridib",
+//   age:5,
+//   address:{
+
+//   }
+// }

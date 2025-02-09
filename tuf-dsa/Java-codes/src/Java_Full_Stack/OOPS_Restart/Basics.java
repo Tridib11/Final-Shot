d5fc7b1fc467da084059776391a45afac45e7e7a
Tridib11 <tridibghosh12345@gmail.com>
2025-02-09 21:56:03 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Basics.java
@@ -0,0 +1,14 @@
+package Java_Full_Stack.OOPS_Restart;
+
+
+class Test{
+    public static void printTest(){
+        System.out.println("Hello");
+    }
+}
+public class Basics {
+    public static void main(String[] args) {
+        Test obj=new Test();
+        obj.printTest();
+    }
+}

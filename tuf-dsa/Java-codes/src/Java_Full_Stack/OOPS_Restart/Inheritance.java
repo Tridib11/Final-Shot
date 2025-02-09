c0c4a6aaec3d832a1241bbf601678b932c6cc3d1
Tridib11 <tridibghosh12345@gmail.com>
2025-02-09 22:47:22 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Inheritance.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Inheritance.java
@@ -1,4 +1,16 @@
 package Java_Full_Stack.OOPS_Restart;
 
+
+class Car{
+    private String numberOfCar;
+
+    public Car(String numberOfCar){
+        this.numberOfCar=numberOfCar;
+    }
+
+    public void printCarNumber(){
+        System.out.println(numberOfCar);
+    }
+}
 public class Inheritance {
 }

162d3d068c95fa020207cdf7a4f81368dd0b04ca
Tridib11 <tridibghosh12345@gmail.com>
2025-02-10 02:54:51 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Testing.java
@@ -0,0 +1,19 @@
+package Java_Full_Stack.OOPS_Restart;
+
+
+class StaticThing{
+    static int a=10;
+}
+public class Testing {
+    public static void main(String[] args) {
+        System.out.println(StaticThing.a);
+
+    }
+
+    private void thing(){
+        System.out.println(StaticThing.a);
+    }
+
+
+
+}

07909b62e471e4b9c8bddbb02a861c1aebfd4be2
Tridib11 <tridibghosh12345@gmail.com>
2025-02-09 22:17:31 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Encapcusaltion.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/Encapcusaltion.java
@@ -1,23 +1,23 @@
 package Java_Full_Stack.OOPS_Restart;
 
 
+class Bank {
 
-class Bank{
+    private int balance = 100;
 
-    private int balance=100;
-
-    public int getBalance(){
+    public int getBalance() {
         return balance;
     }
 
-    public void setBalance(int newBalance){
-        balance=newBalance;
+    public void setBalance(int newBalance) {
+        balance = newBalance;
     }
 }
+
 public class Encapcusaltion {
 
     public static void main(String[] args) {
-        Bank b=new Bank();
+        Bank b = new Bank();
         b.setBalance(200);
         System.out.println(b.getBalance());
     }

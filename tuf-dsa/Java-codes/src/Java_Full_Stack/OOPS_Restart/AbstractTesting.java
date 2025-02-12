14b1ca143fc2c9bfec293d477ad7958328c1dc48
Tridib11 <tridibghosh12345@gmail.com>
2025-02-12 23:09:35 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/OOPS_Restart/AbstractTesting.java
@@ -0,0 +1,18 @@
+package Java_Full_Stack.OOPS_Restart;
+
+
+abstract class engine{
+    public abstract void engine();
+}
+
+class demo extends engine{
+    @Override
+    public void engine() {
+
+    }
+}
+public class AbstractTesting {
+    public static void main(String[] args) {
+
+    }
+}

971def31f5173907170978f42cefd65505c216da
Tridib11 <tridibghosh12345@gmail.com>
2025-02-17 23:05:46 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Exception_Handling/Stack_Trace_Exp.java
@@ -0,0 +1,25 @@
+package Java_Full_Stack.Exception_Handling;
+
+public class Stack_Trace_Exp {
+    public static void main(String[] args) {
+        try{
+            level1();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void level3(){
+        int[] arr=new int[5];
+        arr[5]=1;
+    }
+
+    private static void level2(){
+        level3();
+    }
+
+    private static void level1(){
+        level2();
+    }
+}

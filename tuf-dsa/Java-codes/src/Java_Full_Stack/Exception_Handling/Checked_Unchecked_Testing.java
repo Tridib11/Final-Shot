ec9be7f7cbce699d6f1ec66779b82daf52bda5df
Tridib11 <tridibghosh12345@gmail.com>
2025-02-18 13:52:13 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Exception_Handling/Checked_Unchecked_Testing.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Exception_Handling/Checked_Unchecked_Testing.java
@@ -1,5 +1,7 @@
 package Java_Full_Stack.Exception_Handling;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Checked_Unchecked_Testing {
@@ -7,8 +9,19 @@ public class Checked_Unchecked_Testing {
         readFile("myFile.txt");
     }
 
-    private static void readFile(String fileName){
+    private static void readFile(String fileName) throws FileNotFoundException{
+
+        //one way
+//        try{
+//            FileReader reader=new FileReader(fileName);
+//        }
+//        catch (FileNotFoundException e){
+//            System.out.println("Fle doesnot exist");
+//        }
+
+        //another way to use throws above
         FileReader reader=new FileReader(fileName);
+
 
     }
 }

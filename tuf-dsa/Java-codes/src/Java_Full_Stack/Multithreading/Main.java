940629674bdf6f1831b5ae1f2761d250faf867cc
Tridib11 <tridibghosh12345@gmail.com>
2025-02-19 00:48:17 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading/Main.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading/Main.java
@@ -3,9 +3,9 @@ package Java_Full_Stack.Multithreading;
 public class Main {
     public static void main(String[] args) {
 
-        RunableWorld world=new RunableWorld();
+        RunableWorld world=new RunableWorld(); //new state
         Thread t1=new Thread(world);
-        t1.start();
+        t1.start();//runable state
         for(int i=0;i<100;i++){
             System.out.println(Thread.currentThread().getName());
         }

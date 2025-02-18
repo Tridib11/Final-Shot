b8ab02d9c90f5c15d594350940001022479a9d70
Tridib11 <tridibghosh12345@gmail.com>
2025-02-18 23:45:44 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading/Main.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         World world=new World();
         world.start();
-        for(int i=0;i<10000;i++){
+        for(int i=0;i<100;i++){
             System.out.println(Thread.currentThread().getName());
         }
     }

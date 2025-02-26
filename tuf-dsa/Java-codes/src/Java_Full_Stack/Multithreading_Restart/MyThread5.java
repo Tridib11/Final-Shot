141825b2d1f96d88d4ed247a8225a17c9198eac7
Tridib11 <tridibghosh12345@gmail.com>
2025-02-27 01:21:36 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/MyThread5.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/MyThread5.java
@@ -1,4 +1,28 @@
 package Java_Full_Stack.Multithreading_Restart;
+/*
+The threads that we create
+for using are called User Threads
 
-public class MyThread5 {
+DAEMON Threads are threads that
+run in background
+and JVM doesnot wait for them
+ */
+public class MyThread5 extends Thread {
+    MyThread5(String name){
+        super(name);
+    }
+    @Override
+    public void run() {
+        while(true){
+            System.out.println("Thread Running");
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread5 a=new MyThread5("a");
+        a.setDaemon(true);
+        a.start();
+        System.out.println("Thread Stopped");
+
+    }
 }

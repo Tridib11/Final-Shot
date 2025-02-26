1c77d20abb6b1365172974ea57ab3ebb70189335
Tridib11 <tridibghosh12345@gmail.com>
2025-02-27 00:18:12 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/Test.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/Test.java
@@ -3,7 +3,11 @@ package Java_Full_Stack.Multithreading_Restart;
 public class Test {
     public static void main(String[] args) {
         World w=new World();
-        w.start();
+        //w.start();
+
+        //If implemented Runnable
+        Thread t1=new Thread(w);
+        t1.start();
         for(;;){
             System.out.println(Thread.currentThread().getName());
         }

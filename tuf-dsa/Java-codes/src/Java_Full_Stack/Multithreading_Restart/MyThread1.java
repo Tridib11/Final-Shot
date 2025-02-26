40e7766b0336fd111eb11d61027df0c21a525e2d
Tridib11 <tridibghosh12345@gmail.com>
2025-02-27 00:38:27 +0530

    Java files added

--- tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/MyThread1.java
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/Multithreading_Restart/MyThread1.java
@@ -1,4 +1,19 @@
 package Java_Full_Stack.Multithreading_Restart;
 
-public class MyThread1 {
+public class MyThread1 extends Thread {
+    @Override
+    public void run(){
+       for(int i=1;i<=5;i++){
+           try{
+               Thread.sleep(1000);
+               System.out.println(i);
+           }catch(Exception e){
+               System.out.println(e);
+           }
+       }
+    }
+    public static void main(String[] args) {
+        MyThread1 t1=new MyThread1();
+        t1.start();
+    }
 }

6c6980c5f137709cc434f8f493ac71c035b9708e
Tridib11 <tridibghosh12345@gmail.com>
2025-02-27 20:04:41 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/Java_Full_Stack/JCF_Restart/CustomClass.java
@@ -0,0 +1,18 @@
+package Java_Full_Stack.JCF_Restart;
+
+class Data{
+    public Integer num;
+    public String name;
+    Data(int num,String name){
+        this.num=num;
+        this.name=name;
+    }
+}
+public class CustomClass {
+    public static void main(String[] args) {
+        Data dataObj1=new Data(10,"Tridib");
+        Data dataObj2=new Data(11,"Rohan");
+
+
+    }
+}

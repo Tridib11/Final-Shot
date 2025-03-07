93a18cae903ee2d3a90085b0b77f2c8f56abfcb4
Tridib11 <tridibghosh12345@gmail.com>
2025-03-07 15:01:28 +0530

    Add hoisting example and conditional logging in testing.js

---
+++ Final_ending/week-1/hoisting.js
@@ -0,0 +1,8 @@
+// console.log(a)
+// let a=10
+// console.log(a)
+
+
+console.log(a)
+var a=10
+console.log(a)
---
+++ Final_ending/week-1/testing.js
@@ -0,0 +1,6 @@
+let firstName="rohan"
+let age=10
+let isMarried=true
+
+isMarried?console.log("Yes"):console.log("No")
+

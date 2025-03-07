8cddcb9f68be91b643a233a4621c185e9c837a16
Tridib11 <tridibghosh12345@gmail.com>
2025-03-08 02:20:14 +0530

    Add PrototypalInheritance.js to demonstrate prototypal inheritance in JavaScript

---
+++ Final_ending/OOPS/PrototypalInheritance.js
@@ -0,0 +1,13 @@
+const p1 = {
+  xp1: "I am inside p1",
+};
+const p2 = {
+  xp2: "I am inside p2",
+  __proto__: p1,
+};
+const p3 = {
+  xp3: "I am inside p3",
+  __proto__: p2,
+};
+
+console.log(p3.xp1);

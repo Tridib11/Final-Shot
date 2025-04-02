7cfc76c6d459b53b2dd8cbac6292fe88c4bc5525
Tridib11 <tridibghosh12345@gmail.com>
2025-04-02 22:09:27 +0530

    Add CreateTodo component and Axios dependency; remove unused CSS and index styles

---
+++ Final_ending/week-5/Todo-fullstack/Frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CreateTodo() {
+  return (
+    <div>
+      <input type="text" name="" id="" placeholder='title' />
+      <br />
+      <input type="text" name="" id=""  placeholder='description'/>
+      <button>Add a todo</button>
+    </div>
+  )
+}

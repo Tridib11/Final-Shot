14d1e9e3336cb98a466c37868d6c26fb3cc1afe7
Tridib11 <tridibghosh12345@gmail.com>
2025-04-04 14:14:57 +0530

    Refactor Todo component for improved readability and structure

--- Final_ending/week-6/6.1/6.1/src/Todo.jsx
+++ Final_ending/week-6/6.1/6.1/src/Todo.jsx
@@ -34,21 +34,40 @@ function Todo() {
     },
   ]);
 
-  return <div>
-    <button onClick={()=>{
-      setTodos([...todos,{
-        id:todos.length+1,
-        title:"Some title",
-        description:"Some description"
-      }])
-    }}>Add a Todo</button>
-    {todos.map((todo)=>{
-      return <div>
-      <h1>{todo.title}</h1>
-      <h3>{todo.description}</h3>
-      </div>
-    })}
-  </div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setTodos([
+            ...todos,
+            {
+              id: todos.length + 1,
+              title: "Some title",
+              description: "Some description",
+            },
+          ]);
+        }}
+      >
+        Add a Todo
+      </button>
+      {todos.map((todo) => (
+        <TodoDisplay
+          key={todo.id}
+          title={todo.title}
+          description={todo.description}
+        />
+      ))}
+    </div>
+  );
+}
+function TodoDisplay({ title, description }) {
+  return (
+    <div>
+      <h1>{title}</h1>
+
+      <h3>{description}</h3>
+    </div>
+  );
 }
 
 export default Todo;

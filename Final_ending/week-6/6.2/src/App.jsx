645bd72035d9b2fff50c38f2e8c14e9de52aa3e0
Tridib11 <tridibghosh12345@gmail.com>
2025-04-06 12:23:45 +0530

    Refactor App component to remove unused todos state and simplify Todo rendering

--- Final_ending/week-6/6.2/src/App.jsx
+++ Final_ending/week-6/6.2/src/App.jsx
@@ -1,30 +1,41 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-
+import React, { useState } from 'react'
+import "./App.css"
+import axios from 'axios'
+import { useEffect } from 'react'
 function App() {
-  const [todos, setTodos] = useState([]);
-  useEffect(() => {
-    axios.get("http://localhost:3000/todos").then(function (response) {
-      setTodos(response.data.todos);
-    });
-  }, []);
+  const[id,setId]=useState(1)
   return (
     <div>
-      {todos.map((todo) => (
-        <Todo key={todo.id} title={todo.title} description={todo.description} />
-      ))}
+      <button onClick={()=>{
+        setId(1)
+      }}>1</button>
+      <button onClick={()=>{
+        setId(2)
+      }}>2</button>
+      <button onClick={()=>{
+        setId(3)
+      }}>3</button>
+      <button onClick={()=>{
+        setId(4)
+      }}>4</button>
+      <Todo id={id}/>
     </div>
-  );
+  )
 }
 
-function Todo({ title, description }) {
-  return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{description}</h2>
-    </div>
-  );
+function Todo({id}){
+  const[todo,setTodo]=useState({})
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/todo?id="+id)
+    .then(function(response){
+      setTodo(response.data.todo)
+    })
+  },[id])
+  return <div>
+    <h1>{todo.title}</h1>
+    <h2>{todo.description}</h2>
+  </div>
 }
 
-export default App;
+export default App

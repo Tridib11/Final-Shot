dd4baa00fb0f11707980e5360e501f0dc7e3fe7a
Tridib11 <tridibghosh12345@gmail.com>
2025-04-06 13:31:28 +0530

    refactor: improve Memo component structure and optimize expensive function calculation

--- Final_ending/week-6/6.2/src/Memo.jsx
+++ Final_ending/week-6/6.2/src/Memo.jsx
@@ -1,30 +1,48 @@
-import React, { useMemo, useState } from 'react'
+import React, { useMemo, useState } from "react";
 
 function Memo() {
-  const [text,setText]=useState("")
-  const expensiveFunction=(n)=>{
-    console.log("expensive function rerendered")
-    let total=0
-    for(let i=1;i<n;i++){
-      total+=i
+  const [text, setText] = useState("");
+  const [number,setNumber]=useState(0)
+  const expensiveFunction = (n) => {
+    console.log("expensive function rerendered");
+    let total = 0;
+    for (let i = 1; i < n; i++) {
+      total += i;
     }
-    return total
-  }
+    return total;
+  };
 
-  const sum=useMemo(()=>expensiveFunction(),[]) 
+  const sum = useMemo(() => expensiveFunction(number), [number]);
 
-  console.log("Component Rerendered")
+  console.log("Component Rerendered");
 
   return (
     <div>
-      <input type="text" name="" id="" placeholder='Enter a text' onChange={(e)=>{
-        setText(e.target.value)
-      }} />
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Enter a text"
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
+      <br />
+
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Enter a text"
+        onChange={(e) => {
+          setNumber(e.target.value);
+        }}
+      />
       <br />
 
       <span>Total {sum}</span>
     </div>
-  )
+  );
 }
 
-export default Memo
+export default Memo;

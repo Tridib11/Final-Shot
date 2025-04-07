744ab865c47eed56d3a4ac0a6f369e34394038d1
Tridib11 <tridibghosh12345@gmail.com>
2025-04-07 21:11:59 +0530

    feat: add MemoTest component to demonstrate memoization and state management

---
+++ Final_ending/week-6/6.2/src/MemoTest.jsx
@@ -0,0 +1,25 @@
+import React, {memo, useState } from 'react'
+
+function MemoTest() {
+  const[count,setCount]=useState(0)
+  return (
+    <div>
+      <ButtonComponent/>
+      <button onClick={()=>{
+        setCount(count+1) 
+      }}>Click me {count}</button>
+    </div>
+  )
+}
+
+
+
+const ButtonComponent=memo(()=>{
+  console.log("Button Component rerenderdd")
+
+  return <div>
+      <button>Button CLicked</button>
+  </div>
+})
+
+export default MemoTest
--- Final_ending/week-6/6.2/src/main.jsx
+++ Final_ending/week-6/6.2/src/main.jsx
@@ -4,9 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import Memo from './Memo.jsx'
 import Test from './Test.jsx'
+import MemoTest from './MemoTest.jsx'
 
 createRoot(document.getElementById('root')).render(
   
     // <Memo/>
-    <Test/>
+    <MemoTest/>
 )

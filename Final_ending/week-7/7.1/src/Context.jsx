ac84feda33fd3624c7ce5d3a0a40d74657ea3420
Tridib11 <tridibghosh12345@gmail.com>
2025-04-15 19:34:34 +0530

    feat: add CountContext and implement Context component for state management

---
+++ Final_ending/week-7/7.1/src/Context.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const CountContext=createContext(0)
+function Context() {
+
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <CountContext.Provider value={{ count, setCount }}>
+        <Display/>
+        <Button/>
+      </CountContext.Provider>
+      
+    </div>
+  )
+}
+
+function Display(){
+  const { count } = useContext(CountContext)
+  return <div>
+    {count}
+  </div>
+}
+
+function Button(){
+  const { count, setCount } = useContext(CountContext)
+  return <div>
+    <button onClick={()=>{
+      setCount(count=>count+1)
+    }}>Increase</button>
+
+    <button onClick={()=>{
+      setCount(count=>count-1)
+    }}>Decrease</button>
+  </div>
+}
+
+export default Context

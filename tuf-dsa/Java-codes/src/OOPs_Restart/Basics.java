e52c326817e17656c5c5a066fc7bee320ac2bc12
Tridib11 <tridibghosh12345@gmail.com>
2025-04-19 14:05:04 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/OOPs_Restart/Basics.java
@@ -0,0 +1,13 @@
+package OOPs_Restart;
+
+class Test{
+    public  void printTest(){
+        System.out.println("Printing test");
+    }
+}
+public class Basics {
+    public static void main(String[] args) {
+        Test t=new Test();
+        t.printTest();
+    }
+}

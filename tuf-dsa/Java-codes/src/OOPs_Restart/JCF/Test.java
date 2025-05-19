e74e4e3baec2d26358fc0a66b17a2a9258c0a643
Tridib11 <tridibghosh12345@gmail.com>
2025-05-20 02:33:21 +0530

    Java files added

--- tuf-dsa/Java-codes/src/OOPs_Restart/JCF/Test.java
+++ tuf-dsa/Java-codes/src/OOPs_Restart/JCF/Test.java
@@ -1,5 +1,7 @@
 package OOPs_Restart.JCF;
 
+import java.util.*;
+
 class Data{
     public Integer num;
     public String name;
@@ -10,7 +12,19 @@ class Data{
 }
 public class Test {
     public static void main(String[] args) {
-        Data obj=new Data(12,"Rohan");
+        ArrayList<Integer> alist=new ArrayList<>(); //one ended list
+        alist.add(1);
+        alist.add(2);
+        alist.add(3);
+        alist.add(4);
+        System.out.println(alist);
+        System.out.println(alist.size());
+        System.out.println(alist.get(2));
+        System.out.println(alist.remove(0));
+        alist.add(0,10);
+        System.out.println(alist.contains(10));
+        System.out.println(alist);
+
     }
 
 }

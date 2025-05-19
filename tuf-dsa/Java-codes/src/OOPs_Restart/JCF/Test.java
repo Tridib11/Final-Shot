f632bb5c59e59fb995fe29842f077c77f20f7971
Tridib11 <tridibghosh12345@gmail.com>
2025-05-20 01:51:06 +0530

    Java files added

---
+++ tuf-dsa/Java-codes/src/OOPs_Restart/JCF/Test.java
@@ -0,0 +1,16 @@
+package OOPs_Restart.JCF;
+
+class Data{
+    public Integer num;
+    public String name;
+    Data(int num,String name){
+        this.num=num;
+        this.name=name;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Data obj=new Data(12,"Rohan");
+    }
+
+}

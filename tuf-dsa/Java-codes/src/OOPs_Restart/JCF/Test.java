89c7e0f2c8106e8d2620f7e7f36a045925856069
Tridib11 <tridibghosh12345@gmail.com>
2025-05-20 19:21:44 +0530

    Java files added

--- tuf-dsa/Java-codes/src/OOPs_Restart/JCF/Test.java
+++ tuf-dsa/Java-codes/src/OOPs_Restart/JCF/Test.java
@@ -12,18 +12,28 @@ class Data{
 }
 public class Test {
     public static void main(String[] args) {
-        ArrayList<Integer> alist=new ArrayList<>(); //one ended list
-        alist.add(1);
-        alist.add(2);
-        alist.add(3);
-        alist.add(4);
-        System.out.println(alist);
-        System.out.println(alist.size());
-        System.out.println(alist.get(2));
-        System.out.println(alist.remove(0));
-        alist.add(0,10);
-        System.out.println(alist.contains(10));
-        System.out.println(alist);
+//        ArrayList<Integer> alist=new ArrayList<>(); //one ended list
+//        alist.add(1);
+//        alist.add(2);
+//        alist.add(3);
+//        alist.add(4);
+//        System.out.println(alist);
+//        System.out.println(alist.size());
+//        System.out.println(alist.get(2));
+//        System.out.println(alist.remove(0));
+//        alist.add(0,10);
+//        System.out.println(alist.contains(10));
+//        System.out.println(alist);
+
+        //MUltiEnded list
+
+//        LinkedList<Integer> ll=new LinkedList<>();
+//        ll.add(1);
+//        ll.add(2);
+//        ll.addFirst(0);
+//        ll.addLast(10);
+//        System.out.println(ll.removeLast());
+//        System.out.println(ll);
 
     }
 
